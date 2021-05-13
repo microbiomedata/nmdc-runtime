00142890b5cab9a85894006f91cd06cad1dc4644
Donny Winston <donny@polyneme.xyz>
2021-05-13 17:37:22 -0400

    style: black reformatting

--- metadata-translation/src/bin/fix_omics_processing_json.py
+++ metadata-translation/src/bin/fix_omics_processing_json.py
@@ -1,24 +1,27 @@
 ## add ./lib directory to sys.path so that local modules can be found
 import os, sys, git_root
+
 sys.path.append(os.path.abspath("."))
 sys.path.append(os.path.abspath("./lib"))
 
 import lib.transform_nmdc_data as tx
 
+
 def main(file_path):
-   ## collapse part_of    
-   fixed_json = tx.collapse_json_file(file_path, 'part_of')
-   tx.save_json(fixed_json, file_path)
-   
-   ## collapse has_input
-   fixed_json = tx.collapse_json_file(file_path, 'has_input')
-   tx.save_json(fixed_json, file_path)
-   
-   ## collapse has_output
-   fixed_json = tx.collapse_json_file(file_path, 'has_output')
-   tx.save_json(fixed_json, file_path)
+    ## collapse part_of
+    fixed_json = tx.collapse_json_file(file_path, "part_of")
+    tx.save_json(fixed_json, file_path)
+
+    ## collapse has_input
+    fixed_json = tx.collapse_json_file(file_path, "has_input")
+    tx.save_json(fixed_json, file_path)
+
+    ## collapse has_output
+    fixed_json = tx.collapse_json_file(file_path, "has_output")
+    tx.save_json(fixed_json, file_path)
+
 
 if __name__ == "__main__":
     # main('output/nmdc_etl/test.json') # test file
     # main('output/nmdc_etl/gold_omics_processing.json') # gold omics processing
-    main('output/nmdc_etl/emsl_omics_processing.json') # emsl omics processing
+    main("output/nmdc_etl/emsl_omics_processing.json")  # emsl omics processing
--- metadata-translation/src/bin/mandatory_mixs_terms.py
+++ metadata-translation/src/bin/mandatory_mixs_terms.py
@@ -4,9 +4,12 @@
 
 ## search for lib package and add to path
 import os, sys, re
-subdirs = [os.path.abspath(x[0])
-           for x in os.walk('.')
-           if 'lib' in x[0] and '__pycache__' not in x[0]]
+
+subdirs = [
+    os.path.abspath(x[0])
+    for x in os.walk(".")
+    if "lib" in x[0] and "__pycache__" not in x[0]
+]
 [sys.path.append(p) for p in subdirs]
 
 import click
@@ -18,23 +21,29 @@ def pysqldf(q):
     return sqldf(q, globals())
 
 
-
 @click.command()
 @click.option("--input", "-i", help="name of mixs to process")
 @click.option("--output", "-o", default="", help="name of output file")
 @click.option("--delimiter", "-sep", default="\t", help="specifies delimiter for file")
-@click.option("--sheet-number", "-sheet", default=2,  \
-              help="specifies the sheet number to process for Excel files")
-@click.option("--output-format", "-of", default="tsv", \
-              help="specifies delimiter for the output file; use xls or xlsx output Excel file")
+@click.option(
+    "--sheet-number",
+    "-sheet",
+    default=2,
+    help="specifies the sheet number to process for Excel files",
+)
+@click.option(
+    "--output-format",
+    "-of",
+    default="tsv",
+    help="specifies delimiter for the output file; use xls or xlsx output Excel file",
+)
 
 ## main work starts here
 def main(input, output, delimiter, sheet_number, output_format):
     ## inner function to generate query string
     def get_query_str(data):
         if 1 == sheet_number:
-            q = \
-                """
+            q = """
                 select structured_comment_name, migs_eu, migs_ba, migs_pl, migs_vi, migs_org, me, mimarks_s, mimarks_c,	misag, mimag, miuvig
                 from data
                 where 
@@ -51,8 +60,7 @@ def main(input, output, delimiter, sheet_number, output_format):
                   and miuvig = 'M'
                 """
         elif 2 == sheet_number:
-            q = \
-                """
+            q = """
                 select environmental_package, structured_comment_name, requirement
                 from data
                 where requirement = 'M'
@@ -60,27 +68,28 @@ def main(input, output, delimiter, sheet_number, output_format):
                 """
         else:
             q = ""
-            
+
         return q
-        
+
     file_ext = os.path.splitext(input)[1]
-    if '.xls' in file_ext.strip():
-        data = dop.make_dataframe(input.strip(), file_type='excel', sheet_name=sheet_number)
+    if ".xls" in file_ext.strip():
+        data = dop.make_dataframe(
+            input.strip(), file_type="excel", sheet_name=sheet_number
+        )
     else:
         data = dop.make_dataframe(input, delimiter=delimiter)
 
     query = get_query_str(data)
     df = sqldf(query)
-    
+
     if output:
-        if 'xls' in output_format.strip():
+        if "xls" in output_format.strip():
             df.to_excel(output)
         else:
             df.to_csv(output, sep=delimiter, index=False)
 
 
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-#    import os; print("path: ", os.path.abspath("./lib"))
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
+    #    import os; print("path: ", os.path.abspath("./lib"))
     sys.exit(main())
-
