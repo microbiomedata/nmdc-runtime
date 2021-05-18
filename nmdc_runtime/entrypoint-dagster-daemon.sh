e5e424b88f0a0cab3d8310b197f7598a5cbef997
Donny Winston <donny@polyneme.xyz>
2021-05-18 15:37:10 -0400

    source passes from files

--- nmdc_runtime/entrypoint-dagster-daemon.sh
+++ nmdc_runtime/entrypoint-dagster-daemon.sh
@@ -2,4 +2,28 @@
 
 set -euo pipefail
 
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env "DAGSTER_DEV_MONGO_PASSWORD"
+file_env "DAGSTER_PROD_MONGO_PASSWORD"
+file_env "DAGSTER_DEV_TERMINUS_KEY"
+file_env "DAGSTER_PROD_TERMINUS_KEY"
+file_env "DAGSTER_POSTGRES_PASSWORD"
+
 exec dagster-daemon run
