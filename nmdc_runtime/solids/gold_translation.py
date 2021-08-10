79259a236839ae7602819fcb29578e6f59153c2d
Bill Duncan <wdduncan@gmail.com>
2021-08-10 18:01:21 -0400

    add solids to gold_translation

--- nmdc_runtime/solids/gold_translation.py
+++ nmdc_runtime/solids/gold_translation.py
@@ -26,6 +26,11 @@ from nmdc_runtime.api.models.job import JobOperationMetadata
 from nmdc_runtime.api.models.operation import Operation, ResultT
 from nmdc_runtime.resources.core import RuntimeApiSiteClient
 
+# resources needed for GOLD ETL
+from nmdc_schema import nmdc
+from nmdc_runtime.lib.nmdc_etl_class import NMDC_ETL
+import nmdc_runtime.lib.nmdc_dataframes as nmdc_dataframes
+
 
 def run_and_log(shell_cmd, context):
     process = Popen(shell_cmd, shell=True, stdout=PIPE, stderr=STDOUT)
@@ -108,3 +113,121 @@ def produce_curated_db(context, op: Operation):
     context.log.info(f"{list(nmdc_database.keys())}")
     # TODO do the curation. :)
     return nmdc_database
+
+
+@solid
+def load_merged_data_source(
+    context,
+) -> str:
+    """Create a new data source containing the merged data sources"""
+    spec_file = context.solid_config["spec_file"]
+    mdf = nmdc_dataframes.make_dataframe_from_spec_file(spec_file)
+    print("merged data frame length:", len(mdf))
+
+    # save merged dataframe (mdf)
+    save_path = context.solid_config["save_path"]
+    compression_options = dict(method="zip", archive_name=f"{save_path}")
+    mdf.to_csv(
+        f"{save_path}.zip", compression=compression_options, sep="\t", index=False
+    )
+    return save_path
+
+
+@solid
+def load_nmdc_etl_class(context) -> NMDC_ETL:
+
+    # build instance of NMDC_ETL class
+    etl = NMDC_ETL(
+        merged_data_file=context.solid_config["data_file"],
+        data_source_spec_file=context.solid_config["spec_file"],
+        sssom_file="",
+    )
+    return etl
+
+
+@solid
+def transform_study(context, nmdc_etl: NMDC_ETL) -> tuple:
+    # return {"study_set": nmdc_etl.transform_study()}
+    return ("study_set", nmdc_etl.transform_study())
+
+
+@solid
+def transform_omics_processing(context, nmdc_etl: NMDC_ETL) -> tuple:
+    return ("omics_processing_set", nmdc_etl.transform_omics_processing())
+
+
+@solid
+def transform_biosample(context, nmdc_etl: NMDC_ETL) -> tuple:
+    return ("biosample_set", nmdc_etl.transform_biosample())
+
+
+@solid
+def get_mongo_db(context) -> pymongo.database.Database:
+    host = context.solid_config["host"]
+    username = context.solid_config["username"]
+    password = context.solid_config["password"]
+    dbname = context.solid_config["dbname"]
+
+    client = MongoClient(host=host, username=username, password=password)
+    return client[dbname]
+
+
+@solid
+def load_mongo_collection(context, mongo_db: pymongo.database.Database, data: tuple):
+    collecton_name = data[0]  # get collection name
+    documents = data[1]  # get data portion of tuple
+    collecton = mongo_db[collecton_name]  # get mongo collection
+
+    # drop collection if exists
+    collecton.drop()
+
+    # insert data
+    collecton.insert(documents)
+
+
+### TESTING PIPELINE
+if __name__ == "__main__":
+    from dagster import pipeline
+    from dagster import execute_pipeline
+
+    @pipeline
+    def gold_pipeline1():
+        # load_merged_data_source()
+        db = get_mongo_db()
+        nmdc_etl = load_nmdc_etl_class()
+        gold_study = transform_study(nmdc_etl)
+        gold_omics_processing = transform_omics_processing(nmdc_etl)
+        gold_biosample = transform_biosample(nmdc_etl)
+
+        # load data into mongo
+        load_mongo_collection(db, gold_study)
+        load_mongo_collection(db, gold_omics_processing)
+        load_mongo_collection(db, gold_biosample)
+
+    run_config1 = {
+        "solids": {
+            # "load_merged_data_source": {
+            #     "config": {
+            #         "spec_file": "lib/nmdc_data_source.yaml",
+            #         "save_path": "../data/nmdc_merged_data.tsv.zip",
+            #     }
+            # },
+            "load_nmdc_etl_class": {
+                "config": {
+                    "data_file": "../data/nmdc_merged_data.tsv.zip",
+                    "sssom_map_file": "",
+                    "spec_file": "lib/nmdc_data_source.yaml",
+                }
+            },
+            "get_mongo_db": {
+                "config": {
+                    "host": os.getenv("MONGO_HOST"),
+                    "username": os.getenv("MONGO_USERNAME"),
+                    "password": os.getenv("MONGO_PASSWORD"),
+                    "dbname": "nmdc_etl_staging",
+                }
+            },
+        },
+    }
+
+    result = execute_pipeline(gold_pipeline1, run_config=run_config1)
