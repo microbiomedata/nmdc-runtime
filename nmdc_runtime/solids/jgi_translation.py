c56cfc030f42ac214bdf2cce4f65d2941a0ad8c7
Bill Duncan <wdduncan@gmail.com>
2021-08-18 19:44:50 -0400

    add jgi translation

---
+++ nmdc_runtime/solids/jgi_translation.py
@@ -0,0 +1,109 @@
+from io import BytesIO
+import json
+import os
+from subprocess import Popen, PIPE, STDOUT, CalledProcessError
+from zipfile import ZipFile
+
+import pymongo.database
+from git_root import git_root
+from dagster import (
+    solid,
+    AssetMaterialization,
+    Output,
+    EventMetadata,
+    Failure,
+    OutputDefinition,
+    AssetKey,
+)
+from toolz import get_in
+from pymongo import MongoClient
+
+from nmdc_runtime.api.models.job import JobOperationMetadata
+from nmdc_runtime.api.models.operation import Operation, ResultT
+from nmdc_runtime.resources.core import RuntimeApiSiteClient
+
+# resources needed for ETL
+from nmdc_schema import nmdc
+from nmdc_runtime.lib.nmdc_etl_class import NMDC_ETL
+import nmdc_runtime.lib.nmdc_dataframes as nmdc_dataframes
+
+
+@solid
+def load_nmdc_etl_class(context) -> NMDC_ETL:
+
+    # build instance of NMDC_ETL class
+    etl = NMDC_ETL(
+        merged_data_file=context.solid_config["data_file"],
+        data_source_spec_file=context.solid_config["spec_file"],
+        sssom_file="",
+    )
+    return etl
+
+
+@solid
+def transform_data_object(context, nmdc_etl: NMDC_ETL) -> tuple:
+    return ("jgi.data_object_set", nmdc_etl.transform_jgi_data_object())
+
+
+@solid
+def get_mongo_db(context) -> pymongo.database.Database:
+    host = context.solid_config["host"]
+    username = context.solid_config["username"]
+    password = context.solid_config["password"]
+    dbname = context.solid_config["dbname"]
+
+    client = MongoClient(host=host, username=username, password=password)
+    return client[dbname]
+
+
+@solid
+def load_mongo_collection(context, mongo_db: pymongo.database.Database, data: tuple):
+    collecton_name = data[0]  # get collection name
+    documents = data[1]  # get data portion of tuple
+    collecton = mongo_db[collecton_name]  # get mongo collection
+
+    # drop collection if exists
+    collecton.drop()
+
+    # insert data
+    collecton.insert(documents)
+
+
+### TESTING PIPELINE
+if __name__ == "__main__":
+    from dagster import pipeline
+    from dagster import execute_pipeline
+
+    @pipeline
+    def jgi_pipeline1():
+        # load_merged_data_source()
+        db = get_mongo_db()
+        nmdc_etl = load_nmdc_etl_class()
+        jgi_data_object = transform_data_object(nmdc_etl)
+
+        # load data into mongo
+        load_mongo_collection(db, jgi_data_object)
+
+    run_config1 = {
+        "solids": {
+            "load_nmdc_etl_class": {
+                "config": {
+                    "data_file": git_root(
+                        "metadata-translation/src/data/nmdc_merged_data.tsv.zip"
+                    ),
+                    "sssom_map_file": "",
+                    "spec_file": git_root("nmdc_runtime/lib/nmdc_data_source.yaml"),
+                }
+            },
+            "get_mongo_db": {
+                "config": {
+                    "host": os.getenv("MONGO_HOST"),
+                    "username": os.getenv("MONGO_USERNAME"),
+                    "password": os.getenv("MONGO_PASSWORD"),
+                    "dbname": "nmdc_etl_staging",
+                }
+            },
+        },
+    }
+
+    result = execute_pipeline(jgi_pipeline1, run_config=run_config1)
