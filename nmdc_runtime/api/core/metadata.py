f6fe3eca8047c69284336d725b7d749e6cd04869
Bill Duncan <wdduncan@gmail.com>
2021-09-18 00:26:30 -0400

    add functions for updating mongodb

--- nmdc_runtime/api/core/metadata.py
+++ nmdc_runtime/api/core/metadata.py
@@ -91,7 +91,12 @@ def check_attribute_path(schema, attribute_path):
     return True
 
 
-def update_var_group(data, var_group, schema=None, path_separator="/"):
+##################################
+# functions for updating raw data
+##################################
+
+
+def update_var_group(data, var_group, collection_name=None, path_separator="/") -> dict:
     # split the id group by the group variables
     # var_group[0] -> the variable (if any) in the group_var column
     # var_group[1] -> the dataframe with these variables
@@ -105,8 +110,8 @@ def update_var_group(data, var_group, schema=None, path_separator="/"):
         attribute_path = change_df.loc[i, "path"]
         if len(attribute_path) > 0:
             attribute_path = change_df.loc[i, "path"].split(path_separator)
-            if schema:
-                check_attribute_path(schema, attribute_path)
+            if collection_name:
+                check_attribute_path(collection_name, attribute_path)
             new_val = change_df.loc[i, "value"]
             if len(str(new_val)) > 0:
                 data = assoc_in(data, attribute_path, new_val)
@@ -148,6 +153,11 @@ def update_data(
     return data
 
 
+##################################
+# funcitons to update mongodb
+##################################
+
+
 def make_update_query(collection_name: str, id_val: str, update_values: list):
     update_query = {"update": f"{collection_name}", "updates": []}
 
@@ -161,3 +171,98 @@ def make_update_query(collection_name: str, id_val: str, update_values: list):
 def make_update_query_value(id_val: str, update_value: dict):
     update_dict = {"q": {"id": f"{id_val}"}, "u": {"$set": update_value}}
     return update_dict
+
+
+def make_id_to_collection_dict(mongodb) -> dict:
+    collection_names = [
+        name for name in mongodb.list_collection_names() if name.endswith("_set")
+    ]
+    id_dict = {
+        name: set(mongodb[name].distinct("id"))
+        for name in collection_names
+        if "id_1" in mdb[name].index_information()
+    }
+    return id_dict
+
+
+def get_collection_for_id(id_val, id_map):
+    for collection_name in id_map:
+        if id_val in id_map[collection_name]:
+            return collection_name
+    return None
+
+
+def make_update_var_group(var_group, collection_name=None, path_separator="/") -> list:
+    # split the id group by the group variables
+    # var_group[0] -> the variable (if any) in the group_var column
+    # var_group[1] -> the dataframe with these variables
+    change_df = var_group[1]
+
+    # the grouped dataframes may have indexes that don't
+    # line with the row number, so reset the index
+    change_df = change_df.reset_index(drop=True)
+
+    updates = []  # list of dicts
+    for i in range(len(change_df)):
+        attribute_path = change_df.loc[i, "path"]
+        if len(attribute_path) > 0:
+            attribute_path = change_df.loc[i, "path"].split(path_separator)
+            # if collection_name:
+            #    check_attribute_path(collection_name, attribute_path)
+
+            new_val = change_df.loc[i, "value"]
+            if len(str(new_val)) > 0:
+                # mongodb requries a '.' for nested objects
+                update = {".".join(attribute_path): new_val}
+                updates.append(update)
+
+    return updates
+
+
+def update_mongo_db(
+    df_change: pds.DataFrame,
+    # mongodb,
+    path_separator="/",
+    print_data=False,
+    print_update=False,
+    print_query=False,
+) -> dict:
+
+    # create a dict between collections names and ids
+    id_dict = make_id_to_collection_dict(mongodb)
+
+    # split the change sheet by the group id values
+    id_group = df_change.groupby("group_id")
+
+    updates = []  # list of dicts
+    for ig in id_group:
+        # ig[0] -> id of the data
+        # ig[1] -> dataframe with rows with the id
+
+        #         schema = try_fetching_schema_for_id(ig[0])
+        collection_name = get_collection_for_id(ig[0], id_dict)
+
+        if print_data:
+            data = mongodb[collection_name].find_one({"id": ig[0]})
+            print(data)
+
+        # split the id group by the group variables
+        var_group = ig[1].groupby("group_var")
+        for vg in var_group:
+            update = make_update_var_group(vg, collection_name, path_separator)
+
+            if len(update) > 0:
+                updates.extend(update)
+
+        # update mongo
+        update_query = make_update_query(collection_name, ig[0], updates)
+        status = mongodb.command(update_query)
+
+        if print_query:
+            print(json.dumps(update_query, indent=2))
+
+        if print_update:
+            data = mongodb[collection_name].find_one({"id": ig[0]})
+            print(data)
+
+    return status
