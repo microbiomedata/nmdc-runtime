d0a20a52104216e430853298c0a4038b683ae093
Bill Duncan <wdduncan@gmail.com>
2021-10-01 15:08:24 -0400

    update tests to run with new code

--- tests/test_api/test_metadata.py
+++ tests/test_api/test_metadata.py
@@ -47,8 +47,18 @@ def test_update_01():
     )
     id_ = list(df.groupby("group_id"))[0][0]
     study_doc = dissoc(mdb.study_set.find_one({"id": id_}), "_id")
-    assert study_doc["name"] != "soil study"
-    assert study_doc["ecosystem"] != "soil"
+
+    pi_info = {"has_raw_value": "NEW RAW NAME 2", "name": "NEW PI NAME 2"}
+    assert study_doc["principal_investigator"] != pi_info
+    assert study_doc["name"] != "NEW STUDY NAME 2"
+    assert study_doc["ecosystem"] != "NEW ECOSYSTEM 2"
+    assert study_doc["ecosystem_type"] != "NEW ECOSYSTEM_TYPE 2"
+    assert study_doc["ecosystem_subtype"] != "NEW ECOSYSTEM_SUBTYPE 2"
+
+    website_info = ["HTTP://TEST3.EXAMPLE.COM", "HTTP://TEST4.EXAMPLE.COM"]
+    for website in website_info:
+        assert website not in study_doc.get("websites", [])
+
     update_cmd = mongo_update_command_for(df)
     mdb_scratch = mdb.client["nmdc_runtime_test"]
     copy_docs_in_update_cmd(
@@ -56,9 +66,15 @@ def test_update_01():
     )
     results = update_mongo_db(mdb_scratch, update_cmd)
     first_result = results[0]
-    assert first_result["update_info"]["nModified"] == 9
-    assert first_result["doc_after"]["name"] == "soil study"
-    assert first_result["doc_after"]["ecosystem"] == "soil"
+
+    assert first_result["update_info"]["nModified"] == 12
+    assert first_result["doc_after"]["principal_investigator"] == pi_info
+    assert first_result["doc_after"]["name"] == "NEW STUDY NAME 2"
+    assert first_result["doc_after"]["ecosystem"] == "NEW ECOSYSTEM 2"
+    assert first_result["doc_after"]["ecosystem_type"] == "NEW ECOSYSTEM_TYPE 2"
+    assert first_result["doc_after"]["ecosystem_subtype"] == "NEW ECOSYSTEM_SUBTYPE 2"
+    for website in website_info:
+        assert website in first_result["doc_after"]["websites"]
     assert first_result["validation_errors"] == []
 
 
@@ -69,7 +85,17 @@ def test_changesheet_array_item_nested_attributes():
     )
     id_ = list(df.groupby("group_id"))[0][0]
     study_doc = dissoc(mdb.study_set.find_one({"id": id_}), "_id")
-    assert study_doc.get("has_credit_associations", []) == []
+
+    credit_info = {
+        "applied_role": "Conceptualization",
+        "applies_to_person": {
+            "name": "CREDIT NAME 1",
+            "email": "CREDIT_NAME_1@foo.edu",
+            "orcid": "orcid:0000-0000-0000-0001",
+        },
+    }
+    assert credit_info not in study_doc.get("has_credit_associations", [])
+
     update_cmd = mongo_update_command_for(df)
     mdb_scratch = mdb.client["nmdc_runtime_test"]
     copy_docs_in_update_cmd(
@@ -78,14 +104,32 @@ def test_changesheet_array_item_nested_attributes():
     results = update_mongo_db(mdb_scratch, update_cmd)
     first_doc_after = results[0]["doc_after"]
     assert "has_credit_associations" in first_doc_after
-    assert len(first_doc_after["has_credit_associations"]) == 1
-    assert first_doc_after["has_credit_associations"] == [
-        {
-            "applied_role": "Conceptualization",
-            "applies_to_person": {
-                "name": "Kelly Wrighton",
-                "email": "Kelly.Wrighton@colostate.edu",
-                "orcid": "orcid:0000-0003-0434-4217",
-            },
-        }
-    ]
+    assert credit_info in first_doc_after.get("has_credit_associations", [])
+
+
+def test_update_pi_websites():
+    mdb = get_mongo(run_config_frozen__normal_env).db
+    df = load_changesheet(
+        TEST_DATA_DIR.joinpath("changesheet-update-pi-websites.tsv"), mdb
+    )
+    id_ = list(df.groupby("group_id"))[0][0]
+    study_doc = dissoc(mdb.study_set.find_one({"id": id_}), "_id")
+
+    pi_info = {
+        "has_raw_value": "NEW PI NAME",
+        "name": "NEW PI NAME",
+        "profile_image_url": "https://portal.nersc.gov/NEW-PI-NAME.jpg",
+        "orcid": "orcid:0000-0000-0000-0000",
+        "websites": ["https://www.ornl.gov/staff-profile/NEW-PI-NAME"],
+    }
+    assert study_doc.get("principal_investigator", []) != pi_info
+
+    update_cmd = mongo_update_command_for(df)
+
+    mdb_scratch = mdb.client["nmdc_runtime_test"]
+    copy_docs_in_update_cmd(
+        update_cmd, mdb_from=mdb, mdb_to=mdb_scratch, drop_mdb_to=True
+    )
+    results = update_mongo_db(mdb_scratch, update_cmd)
+    first_result = results[0]
+    assert first_result["doc_after"]["principal_investigator"] == pi_info
