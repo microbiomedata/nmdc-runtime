0e49813220e75cb68d588a4b6c2411788947d7cf
Donny Winston <donny@polyneme.xyz>
2021-11-17 11:49:28 -0500

    feat: add entrypoint for dagit --read-only

---
+++ nmdc_runtime/site/entrypoint-dagit-readonly.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env "MONGO_PASSWORD"
+file_env "TERMINUS_KEY"
+file_env "DAGSTER_POSTGRES_PASSWORD"
+
+exec dagit -h 0.0.0.0 -p 3000 -w workspace.yaml --read-only
