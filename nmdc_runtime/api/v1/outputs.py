d29fa13f9908b8e6804daa407d3f3683287112df
Donny Winston <donny@polyneme.xyz>
2022-09-28 16:34:19 -0400

    feat: use existing type-tagging to trigger job create

--- nmdc_runtime/api/v1/outputs.py
+++ nmdc_runtime/api/v1/outputs.py
@@ -5,8 +5,10 @@ from fastapi import APIRouter, HTTPException, Depends, Response, status
 from pydantic import ValidationError
 from dagster import ExecuteInProcessResult
 from bson import json_util
+from pymongo import ReturnDocument
 from toolz import merge
 from pymongo.errors import DuplicateKeyError
+from pymongo.database import Database as MongoDatabase
 
 from nmdc_runtime.api.models.user import User, get_current_active_user
 from nmdc_runtime.util import unfreeze
@@ -25,6 +27,8 @@ from components.workflow.workflow.core import (
     DataObjectService,
     ReadsQCSequencingActivityService,
 )
+from ..db.mongo import get_mongo_db
+from ..models.object_type import DrsObjectWithTypes
 
 router = APIRouter(prefix="/outputs", tags=["outputs"])
 
@@ -32,17 +36,18 @@ router = APIRouter(prefix="/outputs", tags=["outputs"])
 @router.post(
     "",
     status_code=status.HTTP_201_CREATED,
-    response_model=Dict[str, Any],
+    response_model=DrsObjectWithTypes,
 )
 async def ingest(
     ingest: Ingest,
+    mdb: MongoDatabase = Depends(get_mongo_db),
 ):
     try:
         drs_obj_doc = persist_content_and_get_drs_object(
             content=ingest.json(),
             filename=None,
             content_type="application/json",
-            description="JSON metadata in",
+            description="input metadata for readqc-in wf",
             id_ns="json-readqc-in",
         )
         mgs_service = ReadsQCSequencingActivityService()
@@ -51,27 +56,21 @@ async def ingest(
         activity_dict = [
             member.dict() for member in ingest.read_QC_analysis_activity_set
         ]
-        object_result = [
+        _ = [
             await data_object_service.create_data_object(data_object)
             for data_object in object_dict
         ]
-        activity_result = [
+        _ = [
             await mgs_service.create_mgs_activity(activity)
             for activity in activity_dict
         ]
 
-        # job_spec = {
-        #     "workflow": {"id": "readsqc-1.0.1"},
-        #     "config": {"object_id": drs_obj_doc["id"]},
-        # }
-        # run_config = merge(
-        #     unfreeze(run_config_frozen__normal_env),
-        #     {"ops": {"construct_jobs": {"config": {"base_jobs": [job_spec]}}}},
-        # )
-        # dagster_result: ExecuteInProcessResult = repo.get_job(
-        #     "ensure_jobs"
-        # ).execute_in_process(run_config=run_config)
-        return json.loads(json_util.dumps(drs_obj_doc))
+        doc_after = mdb.objects.find_one_and_update(
+            {"id": drs_obj_doc["id"]},
+            {"$set": {"types": ["readqc-in"]}},
+            return_document=ReturnDocument.AFTER,
+        )
+        return doc_after
 
     except DuplicateKeyError as e:
         raise HTTPException(status_code=409, detail=e.details)
