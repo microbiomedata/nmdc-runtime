8a8856fa14f09e269ecc83d14a4ce1b43a755f98
Patrick Kalita <pkalita@lbl.gov>
2022-12-22 11:30:20 -0800

    Update test to get study ID from context

--- tests/test_ops/test_gold_api_ops.py
+++ tests/test_ops/test_gold_api_ops.py
@@ -21,6 +21,9 @@ def op_context(client_config):
     return build_op_context(
         resources={
             "gold_api_client": gold_api_client_resource.configured(client_config)
+        },
+        op_config={
+            "study_id": "Gs0149396"
         }
     )
 
@@ -29,11 +32,10 @@ def test_gold_biosamples_by_study(client_config, op_context):
     with requests_mock.mock() as mock:
         mock.get(f'{client_config["base_url"]}/biosamples', json=[{ 'biosampleGoldId': 'Gb123456789' }])
         
-        study_id = 'Gs0149396'
-        gold_biosamples_by_study(op_context, study_id)
+        gold_biosamples_by_study(op_context)
 
         assert len(mock.request_history) == 1
-        assert mock.last_request.qs['studygoldid'] == [study_id.lower()]
+        assert mock.last_request.qs['studygoldid'] == ['gs0149396']
         assert mock.last_request.headers['Authorization'].startswith('Basic ')
 
 
@@ -41,9 +43,8 @@ def test_gold_projects_by_study(client_config, op_context):
     with requests_mock.mock() as mock:
         mock.get(f'{client_config["base_url"]}/projects', json=[{ 'projectGoldId': 'Gp123456789' }])
         
-        study_id = 'Gs0149396'
-        gold_projects_by_study(op_context, study_id)
+        gold_projects_by_study(op_context)
 
         assert len(mock.request_history) == 1
-        assert mock.last_request.qs['studygoldid'] == [study_id.lower()]
+        assert mock.last_request.qs['studygoldid'] == ['gs0149396']
         assert mock.last_request.headers['Authorization'].startswith('Basic ')
