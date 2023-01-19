1d016f27e72e9972beabdd6b970ba4fe340074a4
Elais Player <elais@fastmail.com>
2023-01-19 15:24:53 -0700

    add endpoint

---
+++ nmdc_runtime/api/v1/models/workflow_execution_activity.py
@@ -0,0 +1,13 @@
+"""Beans."""
+from typing import List
+
+from nmdc_runtime.workflow_execution_activity import (
+    DataObject, WorkflowExecutionActivity, init_activity_service)
+from pydantic import BaseModel
+
+
+class ActivitySet(BaseModel):
+    """More thought."""
+
+    activity_set: List[WorkflowExecutionActivity]
+    data_object_set: List[DataObject]
