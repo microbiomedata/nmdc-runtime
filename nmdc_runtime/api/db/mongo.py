d99ae7933b5e4a52047af177fe20a00f26eb0714
Donny Winston <donny@polyneme.xyz>
2023-04-03 14:43:00 -0400

    fix: retry mongo replicaset connection on init
    pymongo driver provides pymongo.errors.AutoReconnect signal for this

--- nmdc_runtime/api/db/mongo.py
+++ nmdc_runtime/api/db/mongo.py
@@ -4,9 +4,19 @@ from typing import Set
 
 import pymongo.errors
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from tenacity import wait_random_exponential, retry, retry_if_exception_type
+
 from nmdc_runtime.util import get_nmdc_jsonschema_dict
 from pymongo import MongoClient
 from pymongo.database import Database as MongoDatabase
+
+
+@retry(
+    retry=retry_if_exception_type(pymongo.errors.AutoReconnect),
+    wait=wait_random_exponential(multiplier=0.5, max=60),
+)
+def check_mongo_ok_autoreconnect(mdb: MongoDatabase):
+    return mdb["_runtime.api.allow"].count_documents({}) >= 0
 
 
 @lru_cache
@@ -22,7 +32,10 @@ def get_mongo_db() -> MongoDatabase:
     except pymongo.errors.OperationFailure as e:
         if e.details["codeName"] == "NotYetInitialized":
             _client.admin.command("replSetInitiate")
-    return _client[os.getenv("MONGO_DBNAME")]
+
+    mdb = _client[os.getenv("MONGO_DBNAME")]
+    check_mongo_ok_autoreconnect(mdb)
+    return mdb
 
 
 @lru_cache
