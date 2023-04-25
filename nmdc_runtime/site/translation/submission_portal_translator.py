d01a9898e54371ca25d14f1da5d982f23757a8eb
Patrick Kalita <pkalita@lbl.gov>
2023-04-25 14:21:32 -0700

    Add translation of biosamples to SubmissionPortalTranslator

--- nmdc_runtime/site/translation/submission_portal_translator.py
+++ nmdc_runtime/site/translation/submission_portal_translator.py
@@ -1,13 +1,25 @@
-from typing import List, Union
+import logging
+import re
+from typing import List, Optional, Union
 from nmdc_runtime.site.translation.translator import JSON_OBJECT, Translator
 from nmdc_schema import nmdc
-from toolz import get_in
+from toolz import get_in, groupby, concat
+from importlib import resources
+from linkml_runtime import SchemaView
+from functools import lru_cache
+
+
+@lru_cache
+def _get_schema_view():
+    return SchemaView(str(resources.path('nmdc_schema', 'nmdc_materialized_patterns.yaml')))
+
 
 class SubmissionPortalTranslator(Translator):
     def __init__(self, metadata_submission: JSON_OBJECT = {}, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.metadata_submission = metadata_submission
+        self.schema_view = _get_schema_view()
 
     def _get_pi(self, metadata_submission: JSON_OBJECT) -> Union[nmdc.PersonValue, None]:
         study_form = metadata_submission.get("studyForm")
@@ -42,6 +54,111 @@ class SubmissionPortalTranslator(Translator):
             )
             for contributor in contributors
         ]
+    
+    def _get_gold_study_identifiers(self, metadata_submission: JSON_OBJECT) -> Union[List[str], None]:
+        gold_study_id = get_in(['multiOmicsForm', 'GOLDStudyId'], metadata_submission)
+        if not gold_study_id:
+            return None
+        
+        return [self._get_curie('GOLD', gold_study_id)]
+    
+    def _get_quantity_value(self, raw_value: Optional[str], unit: Optional[str] = None) -> Union[nmdc.QuantityValue, None]:
+        if raw_value is None:
+            return None
+        
+        match = re.fullmatch('([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*)(?:[eE][+-]?\d+)?)(?: *- *([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*)(?:[eE][+-]?\d+)?))?(?: *(\S+))?', raw_value)
+        if not match:
+            return None
+        
+        qv = nmdc.QuantityValue(has_raw_value=raw_value)
+        if match.group(2):
+            # having group 2 means the value is a range like "0 - 1". Either
+            # group 1 or group 2 might be the minimum especially when handling
+            # negative ranges like "0 - -1"
+            num_1 = float(match.group(1))
+            num_2 = float(match.group(2))
+            qv.has_minimum_numeric_value = min(num_1, num_2)
+            qv.has_maximum_numeric_value = max(num_1, num_2)
+        else:
+            # otherwise we just have a single numeric value
+            qv.has_numeric_value = float(match.group(1))
+        
+        if unit:
+            # a unit was manually specified
+            if match.group(3) and unit != match.group(3):
+                # a unit was also found in the raw string; issue a warning
+                # if they don't agree, but keep the manually specified one
+                logging.warning(f'Unit mismatch: "{unit}" and "{match.group(3)}"')
+            qv.has_unit = unit
+        elif match.group(3):
+            # a unit was found in the raw string
+            qv.has_unit = match.group(3)
+
+        return qv
+    
+
+    def _get_ontology_class(self, raw_value: Optional[str]) -> Union[nmdc.OntologyClass, None]:
+        match = re.fullmatch('_*([^\[]+)(?:\[([^\]]+)\])', raw_value)
+        if not match or not match.group(2):
+            logging.warning(f'Could not infer OntologyClass id from value "{raw_value}"')
+            return None
+        
+        return nmdc.OntologyClass(
+            name=match.group(1).strip(),
+            id=match.group(2).strip(),
+        )
+    
+
+    def _get_controlled_identified_term_value(self, raw_value: Optional[str]) -> Union[nmdc.ControlledIdentifiedTermValue, None]:
+        if raw_value is None:
+            return None
+        
+        ontology_class = self._get_ontology_class(raw_value)
+        if ontology_class is None:
+            return None
+        
+        return nmdc.ControlledIdentifiedTermValue(
+            has_raw_value=raw_value,
+            term=ontology_class
+        )
+        
+
+    def _get_controlled_term_value(self, raw_value: Optional[str]) -> Union[nmdc.ControlledTermValue, None]:
+        if raw_value is None:
+            return None
+        
+        value = nmdc.ControlledTermValue(has_raw_value=raw_value)
+        ontology_class = self._get_ontology_class(raw_value)
+        if ontology_class is not None:
+            value.term = ontology_class
+
+        return value
+    
+    
+    def _get_geolocation_value(self, raw_value: Optional[str]) -> Union[nmdc.GeolocationValue, None]:
+        if raw_value is None:
+            return None
+        
+        match = re.fullmatch('([-+]?[1-8]?\d(?:\.\d+)?|90(?:\.0+)?)[\s,]([-+]?180(?:\.0+)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))(?:\.\d+)?)', raw_value)
+        if match is None:
+            return None
+        
+        return nmdc.GeolocationValue(
+            has_raw_value=raw_value,
+            latitude=match.group(1),
+            longitude=match.group(2)
+        )
+    
+    def _get_float(self, raw_value: Optional[str]) -> Union[float, None]:
+        if raw_value is None:
+            return None
+        
+        match = re.match('[+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*)(?:[eE][+-]?\d+)?', raw_value)
+        if not match:
+            return None
+        
+        return float(match.group(0))
+
 
     def _translate_study(
         self, metadata_submission: JSON_OBJECT, nmdc_study_id: str
@@ -60,20 +177,71 @@ class SubmissionPortalTranslator(Translator):
             alternative_names=get_in(['multiOmicsForm', 'alternativeNames'], metadata_submission),
             description=get_in(['studyForm', 'description'], metadata_submission),
             doi=self._get_doi(metadata_submission),
-            gold_study_identifiers=get_in(['multiOmicsForm', 'GOLDStudyId'], metadata_submission),
+            emsl_proposal_identifier=get_in(['multiOmicsForm', 'studyNumber'], metadata_submission),
+            gold_study_identifiers=self._get_gold_study_identifiers(metadata_submission),
             has_credit_associations=self._get_has_credit_associations(metadata_submission),
             id=nmdc_study_id,
-            insdc_bioproject_identifiers=get_in(['multiOmicsForm', 'NCBIBioProjectId'], metadata_submission),
+            insdc_bioproject_identifiers=get_in(['multiOmicsForm', 'NCBIBioProjectId'], metadata_submission) or None,
             name=get_in(['studyForm', 'studyName'], metadata_submission),
             notes=get_in(['studyForm', 'notes'], metadata_submission),
             principal_investigator=self._get_pi(metadata_submission),
+            title=get_in(['studyForm', 'studyName'], metadata_submission),
             websites=get_in(['studyForm', 'linkOutWebpage'], metadata_submission),
         )
+    
+    def _translate_biosample(self, sample_data: List[JSON_OBJECT], nmdc_biosample_id: str, nmdc_study_id: str) -> nmdc.Biosample:
+        slots = {
+            'id': nmdc_biosample_id,
+            'part_of': nmdc_study_id
+        }
+        biosample_slot_names = self.schema_view.class_slots('Biosample')
+        for tab in sample_data:
+            for column, value in tab.items():
+                if column not in biosample_slot_names:
+                    logging.warning(f'No slot {column} on nmdc:Biosample')
+                    continue
+                slot = self.schema_view.induced_slot(column, 'Biosample')
+
+                transformed_value = None
+                if slot.range == 'TextValue':
+                    transformed_value = nmdc.TextValue(has_raw_value=value)
+                elif slot.range == 'QuantityValue':
+                    transformed_value = self._get_quantity_value(value)
+                elif slot.range == 'ControlledIdentifiedTermValue':
+                    transformed_value = self._get_controlled_identified_term_value(value)
+                elif slot.range == 'ControlledTermValue':
+                    transformed_value = self._get_controlled_term_value(value)
+                elif slot.range == 'TimestampValue':
+                    transformed_value = nmdc.TimestampValue(has_raw_value=value)
+                elif slot.range == 'GeolocationValue':
+                    transformed_value = self._get_geolocation_value(value)
+                elif slot.range == 'float':
+                    transformed_value = self._get_float(value)
+                else:
+                    transformed_value = value
+
+                slots[column] = transformed_value
+
+        return nmdc.Biosample(**slots)
+    
 
     def get_database(self) -> nmdc.Database:
         database = nmdc.Database()
 
         nmdc_study_id = self._id_minter("nmdc:Study")[0]
-        database.study_set = [self._translate_study(self.metadata_submission, nmdc_study_id)]
+
+        metadata_submission_data = self.metadata_submission.get('metadata_submission', {})
+        database.study_set = [self._translate_study(metadata_submission_data, nmdc_study_id)]
+
+        sample_data = metadata_submission_data.get('sampleData', {})
+        sample_data_by_id = groupby('source_mat_id', concat(sample_data.values()))
+        nmdc_biosample_ids = self._id_minter("nmdc:Biosample", len(sample_data_by_id))
+        sample_data_to_nmdc_biosample_ids = dict(zip(sample_data_by_id.keys(), nmdc_biosample_ids))
+
+        database.biosample_set = [self._translate_biosample(
+            sample_data, 
+            sample_data_to_nmdc_biosample_ids[sample_data_id],
+            nmdc_study_id
+        ) for sample_data_id, sample_data in sample_data_by_id.items()]
 
         return database
--- tests/test_data/test_submission_portal_translator.py
+++ tests/test_data/test_submission_portal_translator.py
@@ -1,4 +1,9 @@
+import json
+from pathlib import Path
+from nmdc_runtime.api.endpoints.metadata import _validate_json
 from nmdc_runtime.site.translation.submission_portal_translator import SubmissionPortalTranslator
+from linkml_runtime.dumpers import json_dumper
+import requests
 
 
 def test_get_pi():
@@ -67,6 +72,58 @@ def test_get_has_credit_associations():
     assert credit_associations[0].applied_roles is not None
     assert len(credit_associations[0].applied_roles) == 2
     assert credit_associations[0].applied_roles[0].code.text == "Conceptualization"
+
+
+def test_get_quantity_value():
+    translator = SubmissionPortalTranslator()
+    
+    qv = translator._get_quantity_value("3.5")
+    assert qv is not None
+    assert qv.has_raw_value == "3.5"
+    assert qv.has_numeric_value == 3.5
+    assert qv.has_minimum_numeric_value is None
+    assert qv.has_maximum_numeric_value is None
+    assert qv.has_unit is None
+
+    qv = translator._get_quantity_value("0-.1")
+    assert qv is not None
+    assert qv.has_raw_value == "0-.1"
+    assert qv.has_numeric_value is None
+    assert qv.has_minimum_numeric_value == 0
+    assert qv.has_maximum_numeric_value == 0.1
+    assert qv.has_unit is None
+
+    qv = translator._get_quantity_value("1.2 ppm")
+    assert qv is not None
+    assert qv.has_raw_value == "1.2 ppm"
+    assert qv.has_numeric_value == 1.2
+    assert qv.has_minimum_numeric_value is None
+    assert qv.has_maximum_numeric_value is None
+    assert qv.has_unit == "ppm"
+
+    qv = translator._get_quantity_value("98.6F")
+    assert qv is not None
+    assert qv.has_raw_value == "98.6F"
+    assert qv.has_numeric_value == 98.6
+    assert qv.has_minimum_numeric_value is None
+    assert qv.has_maximum_numeric_value is None
+    assert qv.has_unit == "F"
+
+    qv = translator._get_quantity_value("-80", unit="C")
+    assert qv is not None
+    assert qv.has_raw_value == "-80"
+    assert qv.has_numeric_value == -80
+    assert qv.has_minimum_numeric_value is None
+    assert qv.has_maximum_numeric_value is None
+    assert qv.has_unit == "C"
+
+    qv = translator._get_quantity_value("-90 - -100m", unit="meter")
+    assert qv is not None
+    assert qv.has_raw_value == "-90 - -100m"
+    assert qv.has_numeric_value is None
+    assert qv.has_minimum_numeric_value == -100
+    assert qv.has_maximum_numeric_value == -90
+    assert qv.has_unit == "meter"
 
 
 def test_get_dataset(test_minter):
