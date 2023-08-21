86ec6cda73928368244b7f094d0010143130358f
Donny Winston <donny@polyneme.xyz>
2023-08-21 14:43:57 -0400

    rm activity_set_docs specializer

--- nmdc_runtime/util.py
+++ nmdc_runtime/util.py
@@ -444,7 +444,7 @@ class OverlayDB(AbstractContextManager):
 def validate_json(in_docs: dict, mdb: MongoDatabase):
     validator = Draft7Validator(get_nmdc_jsonschema_dict())
     docs = deepcopy(in_docs)
-    docs, validation_errors = specialize_activity_set_docs(docs)
+    validation_errors = {}
 
     known_coll_names = set(nmdc_database_collection_names())
     for coll_name, coll_docs in docs.items():
