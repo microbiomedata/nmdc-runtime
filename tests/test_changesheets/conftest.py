4853c49b57ad78fc555aabf4bdf669bb19cb1cda
Michael Thornton <mbthornton@lbl.gov>
2023-09-06 13:34:00 -0700

    update biosample name unit test
    add explicit expected values

--- tests/test_changesheets/conftest.py
+++ tests/test_changesheets/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 from nmdc_runtime.site.changesheets.changesheet_generator import (
     BaseChangesheetGenerator, ChangesheetLineItem
-)    
+)
 
 from nmdc_runtime.util import REPO_ROOT_DIR
 TEST_DATA_DIR = REPO_ROOT_DIR.joinpath("tests", "test_changesheets", "data")
@@ -13,10 +13,15 @@ TEST_DATA_DIR = REPO_ROOT_DIR.joinpath("tests", "test_changesheets", "data")
 def gold_biosample_response():
     with open(TEST_DATA_DIR.joinpath("gold_biosample_response.json")) as f:
         return json.load(f)
-    
+
 @pytest.fixture
-def gold_biosample_expected_names(gold_biosample_response):
-    return [biosample["biosampleName"].split()[-1] for biosample in gold_biosample_response]
+def gold_biosample_expected_names():
+    return [
+        "DSNY_016-M-37-14-20140409-GEN-DNA1",
+        "GRSM_007-O-20160720-COMP-DNA1",
+        "GUAN_006-M-20161005-COMP-DNA1",
+        "HARV_020-O-20170731-COMP-DNA1",
+    ]
 
 @pytest.fixture
 def base_changesheet_generator():
