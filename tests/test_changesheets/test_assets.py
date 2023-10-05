360584429a5afa0bd98b90f2a79b061a73a177a9
Michael Thornton <mbthornton@lbl.gov>
2023-10-05 13:07:35 -0700

    delete dagster asset based code and tests - move to a demo branch

--- tests/test_changesheets/test_assets.py
+++
@@ -1,9 +0,0 @@
-import mock
-from nmdc_runtime.site.changesheets.assets import (
-    gold_biosamples_for_study,
-    omics_processing_to_biosamples_map,
-)
-from nmdc_runtime.site.changesheets.resources import GoldApiResource, RuntimeApiUserResource
-
-def test_gold_biosamples_for_study():
-    assert True
