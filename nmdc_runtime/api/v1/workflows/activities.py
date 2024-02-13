9eb000dbdd43010122c5c696249795dbc74f79df
Donny Winston <donny@polyneme.xyz>
2024-02-13 14:26:42 -0500

    fix: post_activity to use common `validate_json` utility.
    fixes #478

--- nmdc_runtime/api/v1/workflows/activities.py
+++ nmdc_runtime/api/v1/workflows/activities.py
@@ -1,4 +1,5 @@
 """Module."""
+import os
 from typing import Any
 
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
@@ -13,6 +14,8 @@ from components.nmdc_runtime.workflow_execution_activity import (
 )
 from nmdc_runtime.api.db.mongo import get_async_mongo_db, get_mongo_db
 from nmdc_runtime.api.models.site import Site, get_current_client_site
+from nmdc_runtime.site.resources import MongoDB
+from nmdc_runtime.util import validate_json
 
 router = APIRouter(
     prefix="/workflows/activities", tags=["workflow_execution_activities"]
@@ -26,10 +29,8 @@ async def job_to_db(job_spec: dict[str, Any], mdb: AsyncIOMotorDatabase) -> None
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def post_activity(
     activity_set: dict[str, Any],
-    background_tasks: BackgroundTasks,
     site: Site = Depends(get_current_client_site),
     mdb: MongoDatabase = Depends(get_mongo_db),
-    amdb: AsyncIOMotorDatabase = Depends(get_async_mongo_db),
 ) -> dict[str, str]:
     """Post activity set to database and claim job.
 
@@ -42,15 +43,25 @@ async def post_activity(
     -------
     dict[str,str]
     """
+    _ = site  # must be authenticated
     try:
-        activity_service = ActivityService()
-        nmdc_db = Database(**activity_set)
-        activities = await activity_service.add_activity_set(nmdc_db, mdb)
-        # background_tasks.add_task(
-        #     activity_service.create_jobs, activities, nmdc_db.data_object_set, amdb
-        # )
+        if "activity_set" not in activity_set:
+            raise ValueError("need to supply `activity_set`")
+        activity_set = {"activity_set": activity_set["activity_set"]}
+        rv = validate_json(activity_set, mdb)
+        if rv["result"] == "errors":
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail=str(rv),
+            )
+        mongo_resource = MongoDB(
+            host=os.getenv("MONGO_HOST"),
+            dbname=os.getenv("MONGO_DBNAME"),
+            username=os.getenv("MONGO_USERNAME"),
+            password=os.getenv("MONGO_PASSWORD"),
+        )
+        mongo_resource.add_docs(activity_set, validate=False, replace=True)
         return {"message": "jobs accepted"}
-
     except BulkWriteError as e:
         raise HTTPException(status_code=409, detail=str(e))
     except ValueError as e:
