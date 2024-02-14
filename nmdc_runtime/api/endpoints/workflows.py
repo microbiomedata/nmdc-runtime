fd04d763104f3b6e6e44a590c15c729e99f65707
Jing Cao <jingcao.me@gmail.com>
2024-02-14 03:24:36 +0000

    add workflows/activities endpoint to workflows.py

--- nmdc_runtime/api/endpoints/workflows.py
+++ nmdc_runtime/api/endpoints/workflows.py
@@ -1,13 +1,21 @@
-from typing import List
+import os
+from typing import Any, List
 
 import pymongo
-from fastapi import APIRouter, Depends
+
+from fastapi import APIRouter, Depends, HTTPException
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from pymongo.database import Database as MongoDatabase
+from pymongo.errors import BulkWriteError
 
 from nmdc_runtime.api.core.util import raise404_if_none
-from nmdc_runtime.api.db.mongo import get_mongo_db
+from nmdc_runtime.api.db.mongo import get_mongo_db, activity_collection_names
 from nmdc_runtime.api.models.capability import Capability
 from nmdc_runtime.api.models.object_type import ObjectType
+from nmdc_runtime.api.models.site import Site, get_current_client_site
 from nmdc_runtime.api.models.workflow import Workflow
+from nmdc_runtime.site.resources import MongoDB
+from nmdc_runtime.util import validate_json
 
 router = APIRouter()
 
@@ -43,3 +51,39 @@ def list_workflow_capabilities(
 ):
     doc = raise404_if_none(mdb.workflows.find_one({"id": workflow_id}))
     return list(mdb.capabilities.find({"id": {"$in": doc.get("capability_ids", [])}}))
+
+
+# TODO: Create activity.py in ../models
+@router.post("/workflows/activities")
+async def post_activity(
+    activity_set: dict[str, Any],
+    site: Site = Depends(get_current_client_site),
+    mdb: MongoDatabase = Depends(get_mongo_db),
+):
+    _ = site  # must be authenticated
+    try:
+        # verify activities in activity_set are nmdc-schema compliant
+        for collection_name in activity_set:
+            if collection_name not in activity_collection_names(mdb):
+                raise ValueError("keys must be nmdc-schema activity collection names`")
+        # validate request JSON
+        rv = validate_json(activity_set, mdb)
+        if rv["result"] == "errors":
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail=str(rv),
+            )
+        # create mongodb instance for dagster
+        mongo_resource = MongoDB(
+            host=os.getenv("MONGO_HOST"),
+            dbname=os.getenv("MONGO_DBNAME"),
+            username=os.getenv("MONGO_USERNAME"),
+            password=os.getenv("MONGO_PASSWORD"),
+        )
+        mongo_resource.add_docs(activity_set, validate=False, replace=True)
+        # TODO: Update return value to List[Activity]
+        return {"message": "jobs accepted"}
+    except BulkWriteError as e:
+        raise HTTPException(status_code=409, detail=str(e))
+    except ValueError as e:
+        raise HTTPException(status_code=409, detail=str(e))
