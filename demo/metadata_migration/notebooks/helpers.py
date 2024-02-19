525e25ba467796ae74186c9231893d6efa9bccd6
eecavanna <eecavanna@users.noreply.github.com>
2024-02-19 14:52:47 -0800

    Simplify schema version indicator document in Mongo view

--- demo/metadata_migration/notebooks/helpers.py
+++ demo/metadata_migration/notebooks/helpers.py
@@ -119,25 +119,34 @@ class Config:
         self.transformer_mongo_server_uri = transformer_mongo_server_config["uri"]
 
 
-class MigrationStatus(StrEnum):
+class MigrationEvent(StrEnum):
     r"""
-    Enumeration of all possible migration statuses that can be recorded.
+    Enumeration of all migration events that can be recorded.
     Reference: https://docs.python.org/3/library/enum.html#enum.StrEnum
     """
-    IN_PROGRESS = 'IN_PROGRESS'
-    DONE = 'DONE'
+    MIGRATION_STARTED = 'MIGRATION_STARTED'
+    MIGRATION_COMPLETED = 'MIGRATION_COMPLETED'
 
 
 class Bookkeeper:
-    def __init__(self, mongo_client: MongoClient):
-        r"""Initialize a bookkeeper that can use the specified Mongo client."""
-        self.database = "nmdc"
-        self.collection_name = "_migration_history"
-        self.view_name = "_migration_current_schema_version"
+    def __init__(
+            self,
+            mongo_client: MongoClient,
+            database_name: str = "nmdc",
+            collection_name: str = "_migration_log",
+            view_name: str = "_migration_latest_schema_version",
+    ):
+        r"""Initialize a bookkeeper, which you can use to record migration-related events in a MongoDB database."""
+        self.database_name = database_name
+        self.collection_name = collection_name
+        self.view_name = view_name
 
         # Store references to the database and collection.
-        self.db = mongo_client[self.database]
+        self.db = mongo_client[self.database_name]
         self.collection = self.db.get_collection(self.collection_name)
+
+        # Ensure the MongoDB view that indicates the current schema version, exists.
+        self.ensure_current_schema_version_view_exists()
 
     @staticmethod
     def get_current_timestamp() -> str:
@@ -146,18 +155,25 @@ class Bookkeeper:
         iso_utc_now = utc_now.isoformat()
         return iso_utc_now
 
-    def record_migration_status(self, migrator: MigratorBase, migration_status: MigrationStatus):
-        r"""Records a migration status (along with some metadata) in the collection."""
+    def record_migration_event(self, migrator: MigratorBase, to_schema_version: str, event: MigrationEvent) -> None:
+        r"""
+        Records a migration event in the collection.
+
+        The `to_schema_version` parameter is independent of the `migrator` parameter because, even though the migrator
+        does have a `.get_destination_version()` method, the string returned by that method is the one defined when the
+        migrator was _written_, which is sometimes more generic than the one used for data validation when the migrator
+        is _run_ (e.g. "1.2" as opposed to "1.2.3"). I think the latter value will make more sense to consumers.
+        """
         document = dict(
+            created_at=self.get_current_timestamp(),
+            event=event,
             from_schema_version=migrator.get_origin_version(),
-            to_schema_version=migrator.get_destination_version(),
+            to_schema_version=to_schema_version,
             migrator_module=migrator.__module__,  # name of the Python module in which the `Migrator` class is defined
-            migration_status=migration_status,
-            recorded_at=self.get_current_timestamp(),
         )
         self.collection.insert_one(document)
 
-    def ensure_current_schema_version_view_exists(self):
+    def ensure_current_schema_version_view_exists(self) -> None:
         r"""
         Ensures the MongoDB view that indicates the current schema version, exists.
 
@@ -166,10 +182,29 @@ class Bookkeeper:
         - https://www.mongodb.com/docs/manual/reference/method/db.createView/#mongodb-method-db.createView
         - https://pymongo.readthedocs.io/en/stable/api/pymongo/database.html#pymongo.database.Database.create_collection
         """
-        if self.view_name not in self.db.list_collection_names():  # also lists views
-            agg_pipeline = [{'$sort': {'recorded_at': -1}}, {'$limit': 1}]  # get document having the latest timestamp
+        if self.view_name not in self.db.list_collection_names():  # returns list of names of both collections and views
+            agg_pipeline = [
+                # Sort the documents so the most recent one is first.
+                {'$sort': {'created_at': -1}},
+                # Only preserve the first document.
+                {'$limit': 1},
+                # Derive a simplified document for the view.
+                # Examples: `{ "schema_version": "1.2.3" }` or `{ "schema_version": null }`
+                {
+                    '$project': {
+                        '_id': 0,  # omit the `_id` field
+                        'schema_version': {  # add this field based upon the migration status
+                            '$cond': {
+                                'if': {'$eq': ['$event', MigrationEvent.MIGRATION_COMPLETED]},
+                                'then': '$to_schema_version',  # database conforms to this version of the NMDC Schema
+                                'else': None,  # database doesn't necessarily conform to any version of the NMDC Schema
+                            }
+                        },
+                    }
+                }
+            ]
             self.db.create_collection(name=self.view_name,
                                       viewOn=self.collection_name,
                                       pipeline=agg_pipeline,
                                       check_exists=True,  # only create the view if it doesn't already exist
-                                      comment="The most recent migration status record.")
+                                      comment="The version of the NMDC Schema to which the database conforms.")
