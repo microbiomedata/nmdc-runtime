bab622f2297fff883185fe18af1c3fe40ced9dd8
eecavanna <eecavanna@users.noreply.github.com>
2024-07-16 18:00:25 -0700

    Extract typecode-extraction code into helper function to facilitate testing

--- nmdc_runtime/minter/config.py
+++ nmdc_runtime/minter/config.py
@@ -12,20 +12,54 @@ def minting_service_id() -> str | None:
     return os.getenv("MINTING_SERVICE_ID")
 
 
+def extract_typecode_from_pattern(pattern: str) -> str:
+    r"""
+    Returns the typecode portion of the specified string.
+
+    >>> extract_typecode_from_pattern("foo-123-456$")  # original behavior
+    'foo'
+    >>> extract_typecode_from_pattern("(foo)-123-456$")  # returns first and only typecode
+    'foo'
+    >>> extract_typecode_from_pattern("(foo|bar)-123-456$")  # returns first of 2 typecodes
+    'foo'
+    >>> extract_typecode_from_pattern("(foo|bar|baz)-123-456$")  # returns first of > 2 typecodes
+    'foo'
+    """
+
+    # Get the portion of the pattern preceding the first hyphen.
+    # e.g. "foo-bar-baz" → ["foo", "bar-baz"] → "foo"
+    typecode_sub_pattern = pattern.split("-", maxsplit=1)[0]
+
+    # If that portion of the pattern is enclosed in parentheses, get the portion between the parentheses.
+    # e.g. "(apple|banana|carrot)" → "apple|banana|carrot"
+    if typecode_sub_pattern.startswith("(") and typecode_sub_pattern.endswith(")"):
+        inner_pattern = typecode_sub_pattern[1:-1]
+
+        # Finally, get everything before the first `|`, if any.
+        # e.g. "(apple|banana|carrot)" → "apple"
+        # e.g. "(apple)" → "apple"
+        typecode = inner_pattern.split("|", maxsplit=1)[0]
+    else:
+        # Note: This is the original behavior, before we added support for multi-typecode patterns.
+        # e.g. "foo" → "foo"
+        typecode = typecode_sub_pattern
+
+    return typecode
+
 @lru_cache()
 def typecodes() -> List[dict]:
     r"""
-    Returns a list of typecodes—plus related info—obtained from the schema.
+    Returns a list of dictionaries containing typecodes and other information derived from the schema.
 
-    Preconditions:
+    Preconditions about the schema:
     - The typecode portion of the pattern is between the pattern prefix and the first subsequent hyphen.
     - The typecode portion of the pattern either consists of a single typecode verbatim (e.g. "foo");
       or consists of multiple typecodes in a pipe-delimited list enclosed in parentheses (e.g. "(foo|bar|baz)").
     - The typecode portion of the pattern does not, itself, contain any hyphens.
 
-    TODO: Get typecodes in a different way than by extracting them from a larger string, which seems brittle to me.
+    TODO: Get the typecodes in a different way than by extracting them from a larger string, which seems brittle to me.
           Getting them a different way may require schema authors to _define_ them a different way (e.g. defining them
-          in a dedicated property of a class, named `typecode`).
+          in a dedicated property of a class; for example, one named `typecode`).
     """
     id_pattern_prefix = r"^(nmdc):"
 
@@ -38,26 +72,11 @@ def typecodes() -> List[dict]:
                 # e.g. "^(nmdc):foo-bar-baz" → "foo-bar-baz"
                 pattern_without_prefix: str = p[len(id_pattern_prefix):]
 
-                # Get the portion of the remaining pattern preceding the first hyphen.
-                # e.g. "foo-bar-baz" → ["foo", "bar-baz"] → "foo"
-                typecode_sub_pattern = pattern_without_prefix.split("-", maxsplit=1)[0]
-
-                # If the remaining pattern is enclosed in parentheses, get the portion between them.
-                # e.g. "(apple|banana|carrot)" → "apple|banana|carrot"
-                if typecode_sub_pattern.startswith("(") and typecode_sub_pattern.endswith(")"):
-                    inner_pattern = typecode_sub_pattern[1:-1]
-
-                    # Finally, get everything before the first `|`, if any.
-                    typecode = inner_pattern.split("|", maxsplit=1)[0]
-                else:
-                    # Note: This is the original behavior, before we added support for multi-typecode patterns.
-                    typecode = typecode_sub_pattern
-
                 rv.append(
                     {
                         "id": "nmdc:" + cls_name + "_" + "typecode",
                         "schema_class": "nmdc:" + cls_name,
-                        "name": typecode,
+                        "name": extract_typecode_from_pattern(pattern_without_prefix),
                     }
                 )
             case _:
