85dab162cc5338723bca6cf1fd09114810be939a
Donny Winston <donny@polyneme.xyz>
2024-07-24 13:28:47 +0200

    feat: refactor notebook and supporting code

--- util/mongodump-nmdc.sh
+++ util/mongodump-nmdc.sh
@@ -2,12 +2,11 @@
 # Execute from repo root dir:
 # $ export $(grep -v '^#' .env.localhost.prod | xargs)
 # $ ./util/mongodump-nmdc.sh
-mongodump -h $MONGO_HOST -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase=admin \
+mongodump -v -h $MONGO_HOST -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase=admin \
     -d $MONGO_DBNAME \
     --gzip -o $HOME/nmdcdb-mongodump/nmdcdb/$(date +"%Y-%m-%dT%H")/ \
     --excludeCollectionsWithPrefix="_runtime" \
     --excludeCollectionsWithPrefix="_tmp" \
     --excludeCollectionsWithPrefix="fs." \
     --excludeCollectionsWithPrefix="ids_" \
-    --excludeCollectionsWithPrefix="txn_log" \
-    --excludeCollectionsWithPrefix="functional_annotation_agg"
+    --excludeCollectionsWithPrefix="txn_log"
