fe40072cd7a743896e7f6f50aafad80bbecfda74
Donny Winston <donny@polyneme.xyz>
2024-07-24 13:28:47 +0200

    feat: refactor notebook and supporting code

--- nmdc_runtime/api/db/mongo.py
+++ nmdc_runtime/api/db/mongo.py
@@ -4,15 +4,20 @@ from functools import lru_cache
 from typing import Set, Dict, Any, Iterable
 from uuid import uuid4
 
+from linkml_runtime import SchemaView
+from nmdc_schema.get_nmdc_view import ViewGetter
+from nmdc_schema.nmdc_data import get_nmdc_schema_definition
 from pymongo.errors import OperationFailure, AutoReconnect
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from pydantic import BaseModel, conint
 from tenacity import wait_random_exponential, retry, retry_if_exception_type
 from toolz import concat, merge, unique, dissoc
 
+from nmdc_runtime.config import DATABASE_CLASS_NAME
 from nmdc_runtime.util import (
     get_nmdc_jsonschema_dict,
     schema_collection_names_with_id_field,
+    nmdc_schema_view,
 )
 from pymongo import MongoClient, ReplaceOne
 from pymongo.database import Database as MongoDatabase
@@ -29,12 +34,13 @@ def check_mongo_ok_autoreconnect(mdb: MongoDatabase):
 
 
 @lru_cache
-def get_mongo_db() -> MongoDatabase:
+def get_mongo_db(**extra_kwargs) -> MongoDatabase:
     _client = MongoClient(
         host=os.getenv("MONGO_HOST"),
         username=os.getenv("MONGO_USERNAME"),
         password=os.getenv("MONGO_PASSWORD"),
         directConnection=True,
+        **extra_kwargs,
     )
     mdb = _client[os.getenv("MONGO_DBNAME")]
     check_mongo_ok_autoreconnect(mdb)
@@ -42,27 +48,44 @@ def get_mongo_db() -> MongoDatabase:
 
 
 @lru_cache
-def get_async_mongo_db() -> AsyncIOMotorDatabase:
+def get_async_mongo_db(**extra_kwargs) -> AsyncIOMotorDatabase:
     _client = AsyncIOMotorClient(
         host=os.getenv("MONGO_HOST"),
         username=os.getenv("MONGO_USERNAME"),
         password=os.getenv("MONGO_PASSWORD"),
         directConnection=True,
+        **extra_kwargs,
     )
     return _client[os.getenv("MONGO_DBNAME")]
 
 
-@lru_cache
 def nmdc_schema_collection_names(mdb: MongoDatabase) -> Set[str]:
-    names = set(mdb.list_collection_names()) & set(
-        get_nmdc_jsonschema_dict()["$defs"]["Database"]["properties"]
-    )
-    return names - {
-        "activity_set",
-        "nmdc_schema_version",
-        "date_created",
-        "etl_software_version",
-    }
+    names = set(mdb.list_collection_names()) & set(get_collection_names_from_schema())
+    return {name for name in names if mdb[name].estimated_document_count() > 0}
+
+
+def get_collection_names_from_schema() -> list[str]:
+    """
+    Returns the names of the slots of the `Database` class that describe database collections.
+
+    Source: https://github.com/microbiomedata/refscan/blob/af092b0e068b671849fe0f323fac2ed54b81d574/refscan/lib/helpers.py#L31
+    """
+    collection_names = []
+
+    schema_view = nmdc_schema_view()
+    for slot_name in schema_view.class_slots(DATABASE_CLASS_NAME):
+        slot_definition = schema_view.induced_slot(slot_name, DATABASE_CLASS_NAME)
+
+        # Filter out any hypothetical (future) slots that don't correspond to a collection (e.g. `db_version`).
+        if slot_definition.multivalued and slot_definition.inlined_as_list:
+            collection_names.append(slot_name)
+
+        # Filter out duplicate names. This is to work around the following issues in the schema:
+        # - https://github.com/microbiomedata/nmdc-schema/issues/1954
+        # - https://github.com/microbiomedata/nmdc-schema/issues/1955
+        collection_names = list(set(collection_names))
+
+    return collection_names
 
 
 @lru_cache
