a66c18616a7a0461cf4adb497febd77bdef5a14c
Jing <jing@polyneme.xyz>
2024-08-07 16:00:33 -0400

    remove unused files in `api/v1`

--- nmdc_runtime/api/v1/router.py
+++ nmdc_runtime/api/v1/router.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter
 
 # from . import users
 from . import outputs
-from .workflows import activities
+
+# from .workflows import activities
 
 router_v1 = APIRouter(prefix="/v1", responses={404: {"description": "Not found"}})
-
-router_v1.include_router(activities.router)
