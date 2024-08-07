9ee67dcfee97aa1f823a32117e032a50ab0bf4d5
Jing <jing@polyneme.xyz>
2024-08-07 16:01:12 -0400

    formatting

--- nmdc_runtime/api/v1/router.py
+++ nmdc_runtime/api/v1/router.py
@@ -1,8 +1,3 @@
 from fastapi import APIRouter
 
-# from . import users
-from . import outputs
-
-# from .workflows import activities
-
 router_v1 = APIRouter(prefix="/v1", responses={404: {"description": "Not found"}})
