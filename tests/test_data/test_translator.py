fb915712c5f39e76681b1dbc6d948222c8e46e42
Donny Winston <donny@polyneme.xyz>
2024-11-19 18:42:29 +0100

    chore: black

--- tests/test_data/test_translator.py
+++ tests/test_data/test_translator.py
@@ -9,8 +9,17 @@ class TestTranslator(Translator):
 
 
 def test_ensure_curie():
-    assert TestTranslator._ensure_curie("nmdc:bsm-11-z8x8p723", default_prefix="nmdc") == "nmdc:bsm-11-z8x8p723"
+    assert (
+        TestTranslator._ensure_curie("nmdc:bsm-11-z8x8p723", default_prefix="nmdc")
+        == "nmdc:bsm-11-z8x8p723"
+    )
 
-    assert TestTranslator._ensure_curie("bsm-11-z8x8p723", default_prefix="nmdc") == "nmdc:bsm-11-z8x8p723"
+    assert (
+        TestTranslator._ensure_curie("bsm-11-z8x8p723", default_prefix="nmdc")
+        == "nmdc:bsm-11-z8x8p723"
+    )
 
-    assert TestTranslator._ensure_curie("gold:Gb0123456", default_prefix="nmdc") == "gold:Gb0123456"
+    assert (
+        TestTranslator._ensure_curie("gold:Gb0123456", default_prefix="nmdc")
+        == "gold:Gb0123456"
+    )
