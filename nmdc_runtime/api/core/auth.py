39e04448db96fd7e9096395ed2f16800a7ae00f5
Shreyas Cholia <scholia@lbl.gov>
2024-11-25 03:59:55 -0800

    Update to the newer syntax for getting utcnow() (#788)

--- nmdc_runtime/api/core/auth.py
+++ nmdc_runtime/api/core/auth.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional, Dict
 
 from fastapi import Depends
@@ -101,9 +101,9 @@ def get_password_hash(password):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
