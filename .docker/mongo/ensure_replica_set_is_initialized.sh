50c975743c76f03218bc68f0548e9f32a6bedd72
eecavanna <eecavanna@users.noreply.github.com>
2025-01-28 23:19:16 -0800

    Clarify code comments

--- .docker/mongo/ensure_replica_set_is_initialized.sh
+++ .docker/mongo/ensure_replica_set_is_initialized.sh
@@ -7,8 +7,8 @@
 # instance is a member of a replica set and, if is isn't, (b) creates a replica
 # set of which the local MongoDB instance is the sole member.
 #
-# This shell script was designed to be run on a Docker container running the
-# `mongo:7.0.15` container image.
+# This shell script was designed to be run within a Docker container based upon
+# the `mongo:7.0.15` container image (i.e., https://hub.docker.com/_/mongo).
 #
 # References
 # ----------
@@ -19,6 +19,8 @@
 
 echo '
   try {
+    // Note: If the replica set is not initiated yet, the invoked function will
+    //       throw an error, which will be caught below.
     rs.status();
   } catch (e) {
     rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "localhost" }] });
