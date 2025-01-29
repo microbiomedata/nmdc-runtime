151fccf09778463c1fb6224970c0571c486a4a18
eecavanna <eecavanna@users.noreply.github.com>
2025-01-28 21:12:10 -0800

    Configure MongoDB to run as replica set in local dev/test environments

---
+++ .docker/mongo/ensure_replica_set_is_initialized.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+###############################################################################
+# Overview
+# --------
+# This shell script uses `mongosh` to: (a) check whether the local MongoDB
+# instance is a member of a replica set and, if is isn't, (b) creates a replica
+# set of which the local MongoDB instance is the sole member.
+#
+# This shell script was designed to be run on a Docker container running the
+# `mongo:7.0.15` container image.
+#
+# References
+# ----------
+# 1. https://www.mongodb.com/docs/manual/reference/method/rs.status/
+# 2. https://www.mongodb.com/docs/mongodb-shell/reference/options/
+# 3. https://www.warp.dev/terminus/docker-compose-health-check
+###############################################################################
+
+echo '
+  try {
+    rs.status();
+  } catch (e) {
+    rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "localhost" }] });
+  }
+' | mongosh \
+  --username "${MONGO_INITDB_ROOT_USERNAME}" \
+  --password "${MONGO_INITDB_ROOT_PASSWORD}"
