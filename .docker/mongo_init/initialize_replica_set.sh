6bffccf2320cfe4eaf55035943663dbe2b100cbd
eecavanna <eecavanna@users.noreply.github.com>
2025-02-05 00:33:25 -0800

    Configure replica set via "init container" instead of via healthcheck

--- .docker/mongo_init/initialize_replica_set.sh
+++ .docker/mongo_init/initialize_replica_set.sh
@@ -3,12 +3,19 @@
 ###############################################################################
 # Overview
 # --------
-# This shell script uses `mongosh` to: (a) check whether the local MongoDB
+# This shell script uses `mongosh` to: (a) check whether the specified MongoDB
 # instance is a member of a replica set and, if is isn't, (b) creates a replica
-# set of which the local MongoDB instance is the sole member.
+# set of which that MongoDB instance is the sole member.
 #
 # This shell script was designed to be run within a Docker container based upon
 # the `mongo:7.0.15` container image (i.e., https://hub.docker.com/_/mongo).
+#
+# Positional parameters
+# ---------------------
+# 1. MongoDB hostname (e.g., "mongo")
+# 2. MongoDB port     (e.g., "27017")
+# 3. MongoDB username (e.g., "admin")
+# 4. MongoDB password (e.g., "root")
 #
 # References
 # ----------
@@ -24,5 +31,7 @@ echo '
     rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "localhost" }] });
   }
 ' | mongosh \
-  --username "${MONGO_INITDB_ROOT_USERNAME}" \
-  --password "${MONGO_INITDB_ROOT_PASSWORD}"
+  --host "${1}" \
+  --port "${2}" \
+  --username "${3}" \
+  --password "${4}"
