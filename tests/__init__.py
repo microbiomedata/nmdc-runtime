187153b12e2cea6a44867dc26319b42022bb8b99
eecavanna <eecavanna@users.noreply.github.com>
2025-03-12 23:17:51 -0700

    Update reference to no-longer-existing Dagster class

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,21 @@ import warnings
 
 import dagster
 
-# XXX expecting to remove this on release of dagster 0.13.0
-warnings.filterwarnings("ignore", category=dagster.ExperimentalWarning)
+# Ignore Dagster's beta warnings.
+#
+# Note: This line of code was originally preceeded by a comment saying only:
+#       > "# XXX expecting to remove this on release of dagster 0.13.0"
+#
+# TODO: Consider removing this line of code in accordance with the comment
+#       that originally preceeded it (which is shown above).
+#
+# Note: When we were using Dagster version `1.9.9`, we still had the `category`
+#       kwarg set to `dagster.ExperimentalWarning`. When we updated Dagster
+#       from `1.9.9` to `1.10.4`, we found that `dagter.ExperimentalWarning` no
+#       longer existed. At that point, we changed it to `dagster.BetaWarning`
+#       after seeing this GitHub Discussion comment:
+#       https://github.com/dagster-io/dagster/discussions/13112#discussioncomment-12419858
+#
+# Reference: https://docs.python.org/3/library/warnings.html#warnings.filterwarnings
+#
+warnings.filterwarnings("ignore", category=dagster.BetaWarning)
