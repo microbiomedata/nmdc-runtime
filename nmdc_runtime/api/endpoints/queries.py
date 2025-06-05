250970408aa16fbfa2cf371fcfe9e99f6c858cf3
eecavanna <eecavanna@users.noreply.github.com>
2025-06-04 20:44:40 -0700

    Implement step 4 of documented plan

--- nmdc_runtime/api/endpoints/queries.py
+++ nmdc_runtime/api/endpoints/queries.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from pymongo.database import Database as MongoDatabase
 from toolz import assoc_in, dissoc
 from refscan.lib.Finder import Finder
-from refscan.scanner import identify_referring_documents
+from refscan.scanner import identify_referring_documents, scan_outgoing_references
 
 from nmdc_runtime.api.core.util import now
 from nmdc_runtime.api.db.mongo import (
@@ -407,12 +407,12 @@ def _run_mdb_cmd(cmd: Cmd, mdb: MongoDatabase = _mdb) -> CommandResponse:
         #       3. Perform the user-requested updates within a MongoDB transaction and
         #          leave the transaction in the _pending_ (i.e. not committed) state.
         #       4. For each referring document identified in step 2, call refscan's
-        #          `check_outgoing_references` function to determine whether any of its
+        #          `scan_outgoing_references` function to determine whether any of its
         #          outgoing references were broken by the (pending) "updates". If so,
         #          abort the transaction and raise an HTTP 422 error. Otherwise, continue.
         #          TODO: Account for the possibility that the `id` and/or `type` value
         #                of a referring document was changed by the update operation.
-        #       5. For each "updated" document, call refscan's `check_outgoing_references`
+        #       5. For each "updated" document, call refscan's `scan_outgoing_references`
         #          function to determine whether any of its outgoing references are
         #          broken. If any are, abort the transaction and raise an HTTP 422 error.
         #          Otherwise, abort the transaction and continue with the endpoint's
@@ -447,6 +447,13 @@ def _run_mdb_cmd(cmd: Cmd, mdb: MongoDatabase = _mdb) -> CommandResponse:
             )
         )
 
+        # Make a set of the `_id` values of the target documents so that (later) we can
+        # check whether a given _referring_ document is also one of the _target_ documents
+        # (i.e. is among the documents the user wants to update).
+        target_document_object_ids = set(
+            tdd["_id"] for tdd in target_document_descriptors
+        )
+
         # Identify all documents that reference any of the target documents.
         all_referring_document_descriptors_pre_update = []
         finder = Finder(database=mdb)
@@ -478,8 +485,59 @@ def _run_mdb_cmd(cmd: Cmd, mdb: MongoDatabase = _mdb) -> CommandResponse:
                     ),
                     session=session,
                 )
+                # For each referring document, check whether any of its outgoing references
+                # is broken (in the context of the transaction).
+                for descriptor in all_referring_document_descriptors_pre_update:
+                    referring_document_oid = descriptor["source_document_object_id"]
+                    referring_document_id = descriptor["source_document_id"]
+                    referring_collection_name = descriptor["source_collection_name"]
+                    # If the referring document is among the documents that the user wants to
+                    # update, we skip it, since we will check its outgoing references later.
+                    if referring_document_oid in target_document_object_ids:
+                        continue
+                    # Get the referring document, so we can check its outgoing references.
+                    referring_document = mdb[referring_collection_name].find_one(
+                        {"_id": referring_document_oid},
+                        session=session,
+                    )
+                    # If the referring document is not found, we skip it, since it means that it
+                    # was deleted since we identified it.
+                    if referring_document is None:
+                        continue
+                    violations = scan_outgoing_references(
+                        document=referring_document,
+                        source_collection_name=referring_collection_name,
+                        schema_view=nmdc_schema_view(),
+                        references=get_allowed_references(),
+                        finder=finder,
+                        client_session=session,  # so it uses the pending transaction's session
+                    )
+                    # If any of the references emanating from this document is broken,
+                    # we raise an HTTP 422 error and abort the transaction.
+                    #
+                    # TODO: The violation might not involve a reference to one of the
+                    #       target documents. The `scan_outgoing_references` function
+                    #       scans _all_ references emanating from the document.
+                    #
+                    # TODO: Consider (accumulating and) reporting _all_ would-be-broken references
+                    #       instead of only the _first_ one we encounter.
+                    #
+                    if len(violations) > 0:
+                        raise HTTPException(
+                            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail=(
+                                f"The operation was not performed, because performing it would "
+                                f"have left behind one or more broken references. For example: "
+                                f"The document having 'id'='{referring_document_id}' in "
+                                f"the collection '{referring_collection_name}' has outgoing "
+                                f"references that would be broken by the update operation. "
+                                f"Update or delete referring document(s) and try again."
+                            ),
+                        )
 
-                # TODO: Implement referential integrity checking as described above.
+                # TODO: For each updated document, check whether any of its outgoing references
+                #       is broken (in the context of the transaction).
+                pass
 
                 # Whatever happens, abort the transaction.
                 session.abort_transaction()
