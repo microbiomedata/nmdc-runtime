16054557046a730e5a7378aa933749bd43a33a7b
eecavanna <eecavanna@users.noreply.github.com>
2025-07-01 15:58:59 -0700

    Bump Swagger UI version from 5.9.0 (2023) to 5.25.4 (latest, 2025)

--- nmdc_runtime/api/main.py
+++ nmdc_runtime/api/main.py
@@ -333,8 +333,10 @@ def custom_swagger_ui_html(
         openapi_url=app.openapi_url,
         title=app.title,
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
-        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui.css",
+        # Note: You can check whether a newer version of Swagger UI is available, by visiting:
+        #       https://github.com/swagger-api/swagger-ui/releases
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.25.4/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.25.4/swagger-ui.css",
         swagger_favicon_url="/static/favicon.ico",
         swagger_ui_parameters=swagger_ui_parameters,
     )
