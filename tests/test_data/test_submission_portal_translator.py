77f12c7246f8e076127e1f13d5696bb8294f68fe
James Tessmer <james.tessmer@pnnl.gov>
2025-07-16 12:08:29 -0700

    Correct None assertion to empty list

--- tests/test_data/test_submission_portal_translator.py
+++ tests/test_data/test_submission_portal_translator.py
@@ -114,7 +114,7 @@ def test_get_study_dois():
     assert dois[1].doi_category == DoiCategoryEnum("award_doi")
 
     empty_doi = translator._get_study_dois({})
-    assert empty_doi is []
+    assert empty_doi == []
 
 
 def test_get_has_credit_associations():
