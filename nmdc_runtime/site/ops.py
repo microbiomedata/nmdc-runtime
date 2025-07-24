a48121afd688ca1b781b82fa4ae8d4dcb05eceb6
Sujay Patil <sujaysanjeev.patil@gmail.com>
2025-07-24 13:37:49 -0700

    Make logging more succinct
    Co-authored-by: eecavanna <134325062+eecavanna@users.noreply.github.com>

--- nmdc_runtime/site/ops.py
+++ nmdc_runtime/site/ops.py
@@ -1641,14 +1641,12 @@ def run_script_to_update_insdc_biosample_identifiers(
     update_script = database_updater.queries_run_script_to_update_insdc_identifiers()
 
     if isinstance(update_script, list):
-        total_updates = sum(len(item.get("updates", [])) for item in update_script)
-        context.log.info(
-            f"Generated update script for study {nmdc_study_id} with {total_updates} updates"
-        )
+        total_updates = sum(len(item.get( "updates", [])) for item in update_script)
     else:
-        context.log.info(
-            f"Generated update script for study {nmdc_study_id} with {len(update_script.get('updates', []))} updates"
-        )
+        total_updates = len(update_script.get('updates', []))
+    context.log.info(
+        f"Generated update script for study {nmdc_study_id} with {total_updates} updates"
+    )
 
     return update_script
 
