98b3170d813a02bf9a0c5e80b052b832775f044b
eecavanna <eecavanna@users.noreply.github.com>
2025-07-28 22:56:47 -0700

    Use `@pytest.mark.skip` to skip a single test function

--- tests/test_graphs/test_ensure_jobs.py
+++ tests/test_graphs/test_ensure_jobs.py
@@ -5,7 +5,7 @@ from nmdc_runtime.site.graphs import ensure_jobs
 from nmdc_runtime.site.repository import preset_normal
 
 
-@pytest.skip("Needs supplied state")
+@pytest.mark.skip("Needs supplied state")
 def test_ensure_jobs():
     job = ensure_jobs.to_job(name="test_ensure_jobs", **preset_normal)
     run_config = merge({}, preset_normal["config"])
