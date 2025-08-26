6d45f5e90e4ea4f5dcd5a0422f704b9d20b1030f
eecavanna <eecavanna@users.noreply.github.com>
2025-08-25 18:40:50 -0700

    Omit redundant `Optional` type and refactor pagination decider

--- tests/test_api/test_endpoints_util.py
+++ tests/test_api/test_endpoints_util.py
@@ -2,7 +2,7 @@ from pymongo.database import Database
 import pytest
 
 from nmdc_runtime.api.db.mongo import get_mongo_db
-from nmdc_runtime.api.endpoints.util import does_num_matching_docs_exceed_threshold
+from nmdc_runtime.api.endpoints.util import is_num_matching_docs_within_limit
 from tests.lib.faker import Faker
 
 
@@ -13,8 +13,7 @@ def seeded_db_for_filtered_counting():
     # Seed the database.
     faker = Faker()
     studies = faker.generate_studies(10, title="Test Study")
-    studies[0]["title"] = "Test Study (outlier)"  # one different title
-    filter_ = {"title": {"$regex": "^Test Study"}}
+    filter_ = {"title": "Test Study"}
     assert db["study_set"].count_documents(filter_) == 0
     db["study_set"].insert_many(studies)
     assert db["study_set"].count_documents(filter_) == 10
@@ -25,38 +24,22 @@ def seeded_db_for_filtered_counting():
     db["study_set"].delete_many(filter_)
 
 
-def test_does_num_matching_docs_exceed_threshold(seeded_db_for_filtered_counting: Database):
-    # Seed the database.
+def test_is_num_matching_docs_within_limit(seeded_db_for_filtered_counting: Database):
+    # Confirm the database has been seeded the way we expect.
     db = seeded_db_for_filtered_counting
     collection = db["study_set"]
-    filter_a = {"title": "Test Study"}
-    filter_b = {"title": "Test Study (outlier)"}
-    filter_c = {"title": {"$regex": "^Test Study"}}
-    filter_d = {"title": "Nonexistent Study"}
-    assert collection.count_documents(filter_a) == 9
-    assert collection.count_documents(filter_b) == 1
-    assert collection.count_documents(filter_c) == 10
-    assert collection.count_documents(filter_d) == 0
+    filter_ = {"title": "Test Study"}
+    assert collection.count_documents(filter_) == 10
 
     # Test: Vary the count.
     with pytest.raises(ValueError):
-        assert does_num_matching_docs_exceed_threshold(collection, filter_a, -1)
-    assert does_num_matching_docs_exceed_threshold(collection, filter_a, 0)
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_a, 9)  # there are exactly 9
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_a, 10)
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_a, 11)
+        assert is_num_matching_docs_within_limit(collection, filter_, -1)
+    assert not is_num_matching_docs_within_limit(collection, filter_, 0)
+    assert not is_num_matching_docs_within_limit(collection, filter_, 9)
+    assert is_num_matching_docs_within_limit(collection, filter_, 10)  # there are exactly 10
+    assert is_num_matching_docs_within_limit(collection, filter_, 11)
 
-    # Test: Vary the filter.
-    assert does_num_matching_docs_exceed_threshold(collection, filter_b, 0)
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_b, 1)  # there is exactly 1
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_b, 2)
-    assert does_num_matching_docs_exceed_threshold(collection, filter_c, 9)
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_c, 10)  # there are exactly 10
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_c, 11)
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_d, 0)  # there are exactly 0
-    assert not does_num_matching_docs_exceed_threshold(collection, filter_d, 1)
-
-    # Test: Vary collection.
+    # Test: Vary the collection.
     collection = db["empty_collection"]
     assert collection.count_documents({}) == 0
-    assert not does_num_matching_docs_exceed_threshold(collection, {}, 0)
+    assert is_num_matching_docs_within_limit(collection, {}, 0)
