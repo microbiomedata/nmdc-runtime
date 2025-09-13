0662ca70229153f77b201f750bc4b2afdbc70be1
eecavanna <eecavanna@users.noreply.github.com>
2025-09-12 19:29:06 -0700

    Implement high-level `translate_json_into_multirow_csv` function

--- docs/content/nb/temp.py
+++ docs/content/nb/temp.py
@@ -1,4 +1,6 @@
-def flatten(value: dict | list | str | int | float | bool | None, result: dict, base_json_path: str = "$") -> dict:
+JSONValue = dict[str, "JSONValue"] | list["JSONValue"] | str | int | float | bool | None
+
+def _flatten(value: JSONValue, result: dict, base_json_path: str = "$") -> dict:
     r"""Translates the specified value into a Python dictionary, which can be written to a "single-row" CSV string.
 
     Args:
@@ -21,39 +23,39 @@ def flatten(value: dict | list | str | int | float | bool | None, result: dict, 
       that is the second element (0-indexed) of the array in the `has_input` property of the root value.
     
     # Primitive values:
-    >>> flatten(1, {})  # int
+    >>> _flatten(1, {})  # int
     {'$': 1}
-    >>> flatten(0.1, {})  # float
+    >>> _flatten(0.1, {})  # float
     {'$': 0.1}
-    >>> flatten("potato", {})  # str
+    >>> _flatten("potato", {})  # str
     {'$': 'potato'}
-    >>> flatten(True, {})  # bool
+    >>> _flatten(True, {})  # bool
     {'$': True}
-    >>> flatten(None, {})  # None
+    >>> _flatten(None, {})  # None
     {'$': None}
     
     # Invalid value:
-    >>> flatten(lambda: 123, {})  # function
+    >>> _flatten(lambda: 123, {})  # function
     Traceback (most recent call last):
     ...
     ValueError: The data type of the value is invalid.
 
     # Empty dict and list values:
-    >>> flatten({}, {})  # empty dict
+    >>> _flatten({}, {})  # empty dict
     {}
-    >>> flatten([], {})  # empty list
+    >>> _flatten([], {})  # empty list
     {}
 
     # Non-empty dict and list values:
-    >>> flatten({"a": 1}, {})  # dict having 1 key-value pair
+    >>> _flatten({"a": 1}, {})  # dict having 1 key-value pair
     {'$.a': 1}
-    >>> flatten([1], {})  # list having 1 element
+    >>> _flatten([1], {})  # list having 1 element
     {'$[0]': 1}
 
     # Compound value:
-    >>> flatten([{"a": 1}, {"a": 2, "b": 3}], {})  # list of dicts
+    >>> _flatten([{"a": 1}, {"a": 2, "b": 3}], {})  # list of dicts
     {'$[0].a': 1, '$[1].a': 2, '$[1].b': 3}
-    >>> flatten({"a": [1], "b": [2, 3]}, {})  # dict of lists
+    >>> _flatten({"a": [1], "b": [2, 3]}, {})  # dict of lists
     {'$.a[0]': 1, '$.b[0]': 2, '$.b[1]': 3}
 
     # (Mostly) real-world API response value:
@@ -87,7 +89,7 @@ def flatten(value: dict | list | str | int | float | bool | None, result: dict, 
     ...     ],
     ...     "next_page_token": "nmdc:sys0xvg3j376"
     ... }
-    >>> result = flatten(api_response_body, {})
+    >>> result = _flatten(api_response_body, {})
     >>> pprint(result)
     {'$.next_page_token': 'nmdc:sys0xvg3j376',
      '$.resources[0].ended_at_time': '2024-03-24T17:49:34.756540+00:00',
@@ -111,11 +113,11 @@ def flatten(value: dict | list | str | int | float | bool | None, result: dict, 
     # Else, if the value is a dictionary, process each of its key-value pairs.
     elif isinstance(value, dict):
         for key, val in value.items():
-            result.update(flatten(val, result, f"{base_json_path}.{key}"))
+            result.update(_flatten(val, result, f"{base_json_path}.{key}"))
     # Else, if the value is a list, process each of its elements.
     elif isinstance(value, list):
         for index, val in enumerate(value):
-            result.update(flatten(val, result, f"{base_json_path}[{index}]"))
+            result.update(_flatten(val, result, f"{base_json_path}[{index}]"))
     # Else, raise an exception indicating the value is invalid.
     else:
         raise ValueError("The data type of the value is invalid.")
@@ -123,48 +125,113 @@ def flatten(value: dict | list | str | int | float | bool | None, result: dict, 
     return result
 
 
-def flatten_each(dicts: list[dict]) -> dict:
-    r"""Translates a list of dictionaries into a single flattened dictionary, which can be written to a "multi-row" CSV string.
+def _flatten_dicts(dicts: list[dict[str, JSONValue]]) -> list[dict]:
+    r"""Translates a list of dictionaries into a list of flattened dictionaries.
+
+    The list of flattened dictionaries can be written to a "multi-row" CSV string via an instance of `csv.DictWriter`.
 
     Args:
-        dicts: The list of dictionaries you want to translate, each originally read from a valid JSON string.
+        dicts: The list of dictionaries you want to flatten, each originally read from a valid JSON string.
 
     Returns:
         A Python list that contains the flattened dictionaries, ready to be written to a CSV string via
         an instance of `csv.DictWriter` (see: https://docs.python.org/3/library/csv.html#csv.DictWriter).
 
-    >>> result = flatten_each([
+    >>> result = _flatten_dicts([
     ...     {"a": 1,                                               },
     ...     {"a": 2, "b": 3,                                       },
     ...     {                "c": {"foo": "bar"},                  },
     ...     {                                     "d": [4, None, 5]},
     ... ])
-    >>> result == {
-    ...     '$.a':     [1,    2,    None,  None],
-    ...     '$.b':     [None, 3,    None,  None],
-    ...     '$.c.foo': [None, None, "bar", None],
-    ...     '$.d[0]':  [None, None, None,  4   ],
-    ...     '$.d[1]':  [None, None, None,  None],
-    ...     '$.d[2]':  [None, None, None,  5   ],
-    ... }
+    >>> result == [
+    ...     {'$.a': 1,    '$.b': None, '$.c.foo': None,  '$.d[0]': None, '$.d[1]': None, '$.d[2]': None},
+    ...     {'$.a': 2,    '$.b': 3,    '$.c.foo': None,  '$.d[0]': None, '$.d[1]': None, '$.d[2]': None},
+    ...     {'$.a': None, '$.b': None, '$.c.foo': 'bar', '$.d[0]': None, '$.d[1]': None, '$.d[2]': None},
+    ...     {'$.a': None, '$.b': None, '$.c.foo': None,  '$.d[0]': 4,    '$.d[1]': None, '$.d[2]': 5   },
+    ... ]
     True
     """
 
     # For each dictionary in the list, flatten it.
-    flat_dicts = [flatten(d, {}) for d in dicts]
+    flat_dicts = [_flatten(d, {}) for d in dicts]
 
     # Make a list of all the distinct keys among all the flat dictionaries.
     distinct_keys: set = {key for flat_dict in flat_dicts for key in flat_dict.keys()}
 
-    # Create a new dictionary that has one key-value pair for each distinct key,
-    # and initialize each value to an empty list.
-    result: dict = {distinct_key: [] for distinct_key in distinct_keys}
-
-    # For each flat dictionary, append its values to the corresponding lists,
-    # appending `None` whenever the flat dictionary lacks a given key.
+    # For each flat dictionary, ensure it has each distinct key.
+    # Note: If it lacks a given key, we set the corresponding value to `None`.
     for flat_dict in flat_dicts:
         for distinct_key in distinct_keys:
-            value_from_flat_dict = flat_dict.get(distinct_key, None)
-            result[distinct_key].append(value_from_flat_dict)
+            if distinct_key not in flat_dict:
+                flat_dict[distinct_key] = None
 
-    return result
+    return flat_dicts
+
+
+def translate_json_into_multirow_csv(json_string: str) -> str:
+    r"""Translates a JSON string (consisting of an array of JSON objects) into a "multi-row" CSV string.
+
+    In the resulting CSV string, each column name is a JSONPath expression indicating
+    where that cell's value existed within the original JSON value.
+    
+    Args:
+        json_string: The JSON string you want to translate.
+
+    Returns:
+        A CSV string that represents the data in the JSON string.
+
+    >>> json_string = r'''
+    ... [
+    ...     {"a": 1                                                },
+    ...     {"a": 2, "b": 3                                        },
+    ...     {                "c": {"foo": "bar"}                   },
+    ...     {                                     "d": [4, null, 5]}
+    ... ]
+    ... '''
+    >>> csv_string = translate_json_into_multirow_csv(json_string)
+    >>> print(csv_string, end="")
+    $.a,$.b,$.c.foo,$.d[0],$.d[1],$.d[2]
+    1,,,,,
+    2,3,,,,
+    ,,bar,,,
+    ,,,4,,5
+
+    # Invalid values:
+    >>> translate_json_into_multirow_csv('{"a": [1, 2, 3]}')
+    Traceback (most recent call last):
+    ...
+    ValueError: JSON string must consist of an array of JSON objects.
+    >>> translate_json_into_multirow_csv("[1, 2, 3]")
+    Traceback (most recent call last):
+    ...
+    ValueError: JSON string must consist of an array of JSON objects.
+    """
+
+    import csv
+    import io
+    import json
+
+    # Parse the JSON string into a list of Python dictionaries.
+    dicts = json.loads(json_string)
+
+    if not isinstance(dicts, list) or not all(isinstance(d, dict) for d in dicts):
+        raise ValueError("JSON string must consist of an array of JSON objects.")
+
+    flat_dicts = _flatten_dicts(dicts)
+
+    if len(flat_dicts) == 0:
+        return ""
+    
+    all_keys = flat_dicts[1].keys()
+
+    # Write the flattened dictionary to a CSV string.
+    csv_file_buffer = io.StringIO()
+    writer = csv.DictWriter(
+        csv_file_buffer, fieldnames=sorted(all_keys), lineterminator="\n"
+    )
+    writer.writeheader()
+    writer.writerows(flat_dicts)
+    csv_string = csv_file_buffer.getvalue()
+    csv_file_buffer.close()
+
+    return csv_string
