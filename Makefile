microbiomedata/nmdc-runtime

commit 5d56571b2602306ce61bff65eb0f40108bfd6745

Add migration notebook for nmdc-schema 11.10.0

Makefile
@@ -152,7 +152,7 @@ mongorestore-nmdc-db:
 		${NERSC_USERNAME}@dtn01.nersc.gov:${MONGO_REMOTE_DUMP_DIR}/nmdc/ \
 		/tmp/remote-mongodump/nmdc
 	# Restore from `rsync`ed local directory:
-	mongorestore -v -h localhost:27018 -u admin -p root --authenticationDatabase=admin \
+	mongorestore -v -h localhost:59639 -u admin -p root --authenticationDatabase=admin \
 		--drop --nsInclude='nmdc.*' --gzip --dir /tmp/remote-mongodump
 
 quick-blade:

db/migrations/notebooks/.docker/01-init.js
@@ -1,0 +1,1 @@
+rs.initiate()

commit 0c7ae84541164915fe3dc6b076121c6a4f71d760

hotfix: regression (change of hard-coded `mongorestore` port) in Makefile
Fixes a regression from commit 5d56571b2602306ce61bff65eb0f40108bfd6745 in which a hard-coded port number was changed from 27018 (the default value of `DEV_STACK_HOST_MACHINE_PORT_MONGO` set in `.env.example`) to 59639.

Fix is to explicitly use the value set by `DEV_STACK_HOST_MACHINE_PORT_MONGO` in `.env`, i.e. the locally mapped port for the `mongo` service in `docker-compose.yml`.

Makefile
@@ -96,6 +96,8 @@ nersc-mongo-tunnels:
 		-o ServerAliveInterval=60 \
 		${NERSC_USERNAME}@dtn02.nersc.gov
 
+DEV_STACK_HOST_MACHINE_PORT_MONGO:=$(shell cat .env | grep DEV_STACK_HOST_MACHINE_PORT_MONGO= | cut -d= -f2)
+
 mongorestore-nmdc-db:
 	mkdir -p /tmp/remote-mongodump/nmdc
 	# Optionally, manually update MONGO_REMOTE_DUMP_DIR env var:
@@ -115,7 +117,7 @@ mongorestore-nmdc-db:
 		${NERSC_USERNAME}@dtn01.nersc.gov:${MONGO_REMOTE_DUMP_DIR}/nmdc/ \
 		/tmp/remote-mongodump/nmdc
 	# Restore from `rsync`ed local directory:
-	mongorestore -v -h localhost:59639 -u admin -p root --authenticationDatabase=admin \
+	mongorestore -v -h localhost:$(DEV_STACK_HOST_MACHINE_PORT_MONGO) -u admin -p root --authenticationDatabase=admin \
 		--drop --nsInclude='nmdc.*' --gzip --dir /tmp/remote-mongodump
 
 quick-blade: