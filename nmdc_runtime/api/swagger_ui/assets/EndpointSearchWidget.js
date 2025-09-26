55c03a9fbda028fd92a6746090ba72923ddfeb13
eecavanna <eecavanna@users.noreply.github.com>
2025-09-26 16:26:03 -0700

    Finalize `EndpointSearchWidget.js` source code (clean up)

--- nmdc_runtime/api/swagger_ui/assets/EndpointSearchWidget.js
+++ nmdc_runtime/api/swagger_ui/assets/EndpointSearchWidget.js
@@ -27,54 +27,13 @@ class EndpointSearchWidget extends HTMLElement {
         // Reference: https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_shadow_DOM#creating_a_shadow_dom
         this.attachShadow({ mode: "open" });
 
-        // Implement the widget.
-        const containerEl = document.createElement("div");
-        const innerContainerEl = document.createElement("div");
-        const inputEl = document.createElement("input");
-        this.resultsPanelEl = document.createElement("div");
-        this.resultsListEl = document.createElement("ul");
-        containerEl.classList = "container";
-        innerContainerEl.classList = "inner-container";
-        this.resultsPanelEl.classList = "results-panel";
-        inputEl.name = "search-term";
-        inputEl.placeholder = "Find an endpoint...";
-        this.resultsPanelEl.appendChild(this.resultsListEl);
-        innerContainerEl.appendChild(inputEl);
-        innerContainerEl.appendChild(this.resultsPanelEl);
-        containerEl.appendChild(innerContainerEl);
-
-        // Make an array of all the endpoints that Swagger UI knows about. This will be our search index.
+        // Add styles that will be applied to the widget.
         //
-        // Note: In an earlier implementation of this step, we queried the DOM for this information.
-        //       However, that didn't work when any of the endpoint groups were collapsed. So, instead,
-        //       we access the data structure returned by the `SwaggerUI()` constructor, which is
-        //       invoked by the JavaScript code built into FastAPI (that JavaScript code assigns
-        //       the return value to a global variable named `ui`, which we access here).
+        // Note: We chose the specific style rules shown below in an attempt to
+        //       match the "look and feel" of the Swagger UI page. At least some
+        //       of the seemingly-arbitrarily-chosen values were copied verbatim
+        //       from native Swagger UI elements (via a web browser's DevTools).
         //
-        // TODO: Consider extracting the endpoint's description, too, so we can display a portion of it
-        //       in the search results. It can be obtained via `opMap.get("operation").get("description")`.
-        //
-        const operationMaps = Array.from(ui.specSelectors.operations());
-        this.endpoints = operationMaps.map(opMap => ({
-            urlPath: opMap.get("path"), // e.g. "/studies/{study_id}"
-            httpMethod: opMap.get("method").toUpperCase(), // e.g. "GET"
-            operationId: opMap.get("operation").get("operationId"), // e.g. "find_studies_studies_get"
-            tag: opMap.get("operation").get("tags").get(0), // e.g. "Metadata access: Find"
-        }));
-        console.debug(`Found ${this.endpoints.length} endpoints in OpenAPI schema`);
-
-        // Make it so the search results update whenever the value of the search input
-        // changes as the result of a user action (e.g. typing, cutting, pasting).
-        // Reference: https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
-        inputEl.addEventListener("input", (event) => {
-            this.updateSearchResults(event.target.value);
-        });
-
-        // Mount the container element to the custom element's Shadow DOM tree.
-        this.shadowRoot.appendChild(containerEl);
-
-        // Add styles to the custom element's Shadow DOM tree.
-        // Note: We try to mimic the appearance of native Swagger UI elements.
         const styleEl = document.createElement("style");
         styleEl.textContent = `
             .container {
@@ -131,10 +90,57 @@ class EndpointSearchWidget extends HTMLElement {
         `;
         this.shadowRoot.appendChild(styleEl);
 
-        // Initialize the search results to be empty.
-        this.updateSearchResults("");
+        // Implement the base HTML structure of the widget.
+        const containerEl = document.createElement("div");
+        containerEl.classList = "container";
+        containerEl.innerHTML = String.raw`
+            <div class="inner-container">
+                <input name="search-term" placeholder="Find an endpoint..." />
+                <div class="results-panel">
+                    <ul class="results-list"></ul>
+                </div>
+            </div>
+        `;
+        this.inputEl = containerEl.querySelector("input");
+        this.resultsPanelEl = containerEl.querySelector(".results-panel");
+        this.resultsListEl = containerEl.querySelector(".results-list");
+
+        // Make an array of all the endpoints that Swagger UI knows about. This will be our search index.
+        //
+        // Note: In an earlier implementation of this step, we queried the DOM for this information.
+        //       However, that didn't work when any of the endpoint groups were collapsed. So, instead,
+        //       we access the data structure returned by the `SwaggerUI()` constructor, which is
+        //       invoked by the JavaScript code built into FastAPI (that JavaScript code assigns
+        //       the return value to a global variable named `ui`, which we access here).
+        //
+        // TODO: Consider extracting the endpoint's description, too, so we can display a portion of it
+        //       in the search results. It can be obtained via `opMap.get("operation").get("description")`.
+        //
+        const operationMaps = Array.from(ui.specSelectors.operations());
+        this.endpoints = operationMaps.map(opMap => ({
+            urlPath: opMap.get("path"), // e.g. "/studies/{study_id}"
+            httpMethod: opMap.get("method").toUpperCase(), // e.g. "GET"
+            operationId: opMap.get("operation").get("operationId"), // e.g. "find_studies_studies_get"
+            tag: opMap.get("operation").get("tags").get(0), // e.g. "Metadata access: Find"
+        }));
+        console.debug(`Found ${this.endpoints.length} endpoints in OpenAPI schema`);
+
+        // Make it so the search results list gets updated whenever the user changes the search input.
+        // Reference: https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
+        this.inputEl.addEventListener("input", (event) => {
+            const inputValue = event.target.value;
+            this.updateSearchResults(inputValue);
+        });
+
+        // Mount the widget's HTML element tree to the Shadow DOM tree.
+        this.shadowRoot.appendChild(containerEl);
     }
 
+    /**
+     * Updates the search results list so it shows the endpoints whose URL paths contain the search term.
+     * 
+     * @param {string} searchTerm The search term.
+     */
     updateSearchResults(searchTerm) {
         // Special case: If the search term is empty, clear the search results.
         if (searchTerm.trim().length === 0) {
@@ -142,8 +148,10 @@ class EndpointSearchWidget extends HTMLElement {
             return;
         }
 
-        // Identify the matching endpoints (case-insensitively).
+        // Lowercase the search term to enable case-insensitive comparison.
         const lowercaseSearchTerm = searchTerm.toLowerCase();
+        
+        // Make a list of all endpoints whose URL paths contains the search term.
         const matchingEndpoints = this.endpoints.filter(item => {
             const lowercaseUrlPath = item.urlPath.toLowerCase();
             return lowercaseUrlPath.includes(lowercaseSearchTerm);
@@ -156,10 +164,9 @@ class EndpointSearchWidget extends HTMLElement {
             return;
         }
 
-        // Update the search results to list the matching endpoints.
+        // Build a deep link for each matching endpoint (sorted by URL path, then HTTP method).
+        // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
         const resultEls = matchingEndpoints.sort((a, b) => {
-            // If the URL paths are identical, sort by HTTP method; otherwise, sort by URL path.
-            // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
             if (a.urlPath.localeCompare(b.urlPath) === 0) {
                 return a.httpMethod.localeCompare(b.httpMethod);
             } else {
@@ -169,33 +176,28 @@ class EndpointSearchWidget extends HTMLElement {
             const liEl = document.createElement("li");
             const aEl = document.createElement("a");
 
-            // Here, we distinguish the part of the URL path that matched the search term.
+            // Wrap the part of the URL path that matches the search term, so that we can
+            // style it differently from the rest of the URL path.
             const urlPathSpanEl = document.createElement("span");
             const lowercaseUrlPath = matchingEndpoint.urlPath.toLowerCase();
             const substrCharIdx = lowercaseUrlPath.indexOf(lowercaseSearchTerm);
-            if (substrCharIdx === -1) {
-                // Note: I don't expect this to ever happen, since we already know this
-                //       endpoint matches the search term.
-                urlPathSpanEl.textContent = matchingEndpoint.urlPath;
-            } else {
-                const substrSpanEl = document.createElement("span");
-                const preSubstrChars = matchingEndpoint.urlPath.substring(0, substrCharIdx);
-                const substrChars = matchingEndpoint.urlPath.substring(substrCharIdx, substrCharIdx + searchTerm.length);
-                const postSubstrChars = matchingEndpoint.urlPath.substring(substrCharIdx + searchTerm.length);
-                substrSpanEl.classList = "matching-substring";
-                substrSpanEl.textContent = substrChars;
-                urlPathSpanEl.append(preSubstrChars, substrSpanEl, postSubstrChars);
-            }
+            const substrSpanEl = document.createElement("span");
+            const preSubstrChars = matchingEndpoint.urlPath.substring(0, substrCharIdx);
+            const substrChars = matchingEndpoint.urlPath.substring(substrCharIdx, substrCharIdx + searchTerm.length);
+            const postSubstrChars = matchingEndpoint.urlPath.substring(substrCharIdx + searchTerm.length);
+            substrSpanEl.classList = "matching-substring";
+            substrSpanEl.textContent = substrChars;
+            urlPathSpanEl.append(preSubstrChars, substrSpanEl, postSubstrChars);
 
-            // Here, we build a "deep link" to the corresponding endpoint, using the syntax
-            // shown in the Swagger UI "Deep Linking" documentation, at:
+            // Build a Swagger UI-compliant "deep link" to the corresponding endpoint,
+            // using the syntax shown in the Swagger UI "Deep Linking" documentation, at:
             // https://swagger.io/docs/open-source-tools/swagger-ui/usage/deep-linking/#usage
             //
             // Note: The reason we don't use something like `scrollTo` or `scrollIntoView`
             //       is that the target element may not be mounted to the DOM right now,
             //       due to it being within a collapsed section. Instead, we send the
-            //       browser to the deep link URL (Swagger UI will automatically expand
-            //       the relevant section when the page loads at that deep link URL).
+            //       browser to the "deep link" URL (Swagger UI will automatically expand
+            //       the relevant section when the page loads that "deep link" URL).
             //
             const urlWithoutQueryStr = window.location.origin + window.location.pathname;
             const tagPart = encodeURIComponent(matchingEndpoint.tag);
@@ -212,6 +214,6 @@ class EndpointSearchWidget extends HTMLElement {
     }
 }
 
-// Register a custom HTML element (`<endpoint-search-widget>`).
+// Register a custom HTML element (i.e. `<endpoint-search-widget>`).
 // Docs: https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements#registering_a_custom_element
 customElements.define("endpoint-search-widget", EndpointSearchWidget);
