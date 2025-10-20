ff48fdc57f4f3245e8e12e71c91c94d9d91f18c3
olivia.hess <olivia.hess@pnnl.gov>
2025-10-20 15:38:23 -0400

    need to add auth

--- nmdc_runtime/api/endpoints/wf_file_staging.py
+++ nmdc_runtime/api/endpoints/wf_file_staging.py
@@ -3,17 +3,16 @@ import json
 from fastapi import APIRouter, Depends
 from pymongo.database import Database
 from typing import Optional, Annotated
+from fastapi import APIRouter, Depends, Query
+from toolz import merge
 
+from nmdc_runtime.api.core.util import raise404_if_none, HTTPException, status
 from nmdc_runtime.api.db.mongo import get_mongo_db
 from nmdc_runtime.api.models.util import ListRequest, ListResponse
-from nmdc_runtime.api.models.nmdc_schema import (
-    DataObject,
-    DataObjectListRequest,
-    list_request_filter_to_mongo_filter,
-)
+from nmdc_runtime.api.endpoints.util import list_resources
 from nmdc_runtime.api.models.site import (
     Site,
-    maybe_get_current_client_site,
+    get_current_client_site,
 )
 from nmdc_runtime.api.models.wfe_file_stages import Globus
 
@@ -24,8 +23,26 @@ router = APIRouter()
 )
 def list_globus_records(
     req: Annotated[ListRequest, Query()],
-    mdb: Database = Depends(get_mongo_db),
-    maybe_site: Optional[Site] = Depends(maybe_get_current_client_site),
+    mdb: Database = Depends(get_mongo_db)
 ):
-    pass
+    return list_resources(req, mdb, "globus")
 
+
+@router.get("/globus/{task_id}", response_model=ListResponse[Globus])
+def get_globus(
+    task_id: str,
+    mdb: Database = Depends(get_mongo_db),
+):
+    op = raise404_if_none(mdb.globus.find_one({"task_id": task_id}))
+    return op
+
+@router.patch("/globus/{task_id}", response_model=Globus)
+def update_object(
+    object_id: str,
+    object_patch: Globus,
+    mdb: Database = Depends(get_mongo_db),
+):
+    doc = raise404_if_none(mdb.globus.find_one({"id": object_id}))
+    doc_globus_patched = merge(doc, object_patch.model_dump(exclude_unset=True))
+    mdb.globus.replace_one({"id": object_id}, doc_globus_patched)
+    return doc_globus_patched
