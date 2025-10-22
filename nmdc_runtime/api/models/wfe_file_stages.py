2ff3203ab78c4406e468680c22c4a6b37f11b9b8
mflynn <mflynn@lanl.gov>
2025-10-22 09:06:44 -0600

    added model for SequencingProject

--- nmdc_runtime/api/models/wfe_file_stages.py
+++ nmdc_runtime/api/models/wfe_file_stages.py
@@ -8,3 +8,13 @@ class Globus(BaseModel):
 
     task_id: str
     task_status: str
+
+
+class SequencingProject(BaseModel):
+    """
+    Represents metadata for a sequencing project.
+    """
+    project_name: str
+    proposal_id: str
+    nmdc_study_id: str
+    analysis_projects_dir: str
