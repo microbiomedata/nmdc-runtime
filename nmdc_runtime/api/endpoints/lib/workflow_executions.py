b18213c72417df1cd230a1d7bd61d2053104da68
eecavanna <eecavanna@users.noreply.github.com>
2026-02-21 21:57:46 -0800

    Update helper function to remove obsolete `superseded_by` field from WFE

--- nmdc_runtime/api/endpoints/lib/workflow_executions.py
+++ nmdc_runtime/api/endpoints/lib/workflow_executions.py
@@ -12,7 +12,7 @@ def remove_from_supersession_chain(
     """
     Removes the specified `WorkflowExecution` from any `superseded_by` chain(s) it belongs to,
     updating the `superseded_by` fields of upstream `WorkflowExecution`s accordingly.
-    
+
     Examples (where A/B/C represent `WorkflowExecution`s and `→` represents a `superseded_by` field):
     - If "A → B → C" and we remove "A", the result will be "B → C".
     - If "A → B → C" and we remove "B", the result will be "A → C".
@@ -23,13 +23,18 @@ def remove_from_supersession_chain(
     :param db: Database instance to use for updating `superseded_by` fields.
     :param client_session: Optional `ClientSession`, which the function will use to perform the
                            database operations within the existing MongoDB transaction, if any.
-    
+
     Reference: https://microbiomedata.github.io/nmdc-schema/superseded_by/
     """
 
     # Get the `id` of the `WorkflowExecution`, if any, that supersedes the subject one.
     wfe_id = workflow_execution["id"]
     wfe_superseding_subject_wfe = workflow_execution["superseded_by"] if "superseded_by" in workflow_execution else None
+
+    # Remove the `superseded_by` field, if any, from the subject `WorkflowExecution`,
+    # effectively removing it from the chain, if any.
+    wfe_set = db.get_collection("workflow_execution_set")
+    wfe_set.update_one({"id": wfe_id}, {"$unset": {"superseded_by": ""}}, session=client_session)
 
     # Update the `superseded_by` fields to reflect the removal of the subject WFE from the chain.
     #
@@ -40,7 +45,6 @@ def remove_from_supersession_chain(
     # all WFEs that are superseded by the subject one, so they contain the `id` of the WFE that
     # supersedes the subject one.
     #
-    wfe_set = db.get_collection("workflow_execution_set")
     if wfe_superseding_subject_wfe is None:
         operation = {"$unset": {"superseded_by": ""}}
     else:
