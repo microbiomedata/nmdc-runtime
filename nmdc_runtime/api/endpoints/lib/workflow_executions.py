9532fd4055757ac1696c0264ac33bd52aa17418a
eecavanna <eecavanna@users.noreply.github.com>
2026-02-23 21:55:54 -0800

    Refrain from modifying subject WFE or its DataObjects

--- nmdc_runtime/api/endpoints/lib/workflow_executions.py
+++ nmdc_runtime/api/endpoints/lib/workflow_executions.py
@@ -4,23 +4,32 @@ from pymongo.client_session import ClientSession
 from pymongo.database import Database
 
 
-def remove_from_supersession_chain(
+def prepare_supersession_chain_for_workflow_execution_deletion(
     workflow_execution: dict,
     db: Database,
     client_session: Optional[ClientSession] = None,
 ) -> None:
     """
-    Removes the specified `WorkflowExecution` from any `superseded_by` chain(s) it belongs to,
-    updating the `superseded_by` fields of upstream `WorkflowExecution`s and `DataObject`s
-    accordingly.
+    Updates or deletes the `superseded_by` field of each `WorkflowExecution` and `DataObject`
+    whose `superseded_by` field currently points to the subject `WorkflowExecution` (i.e.
+    the one you are preparing to delete), so that that field no longer points to it.
 
-    Examples (where A/B/C represent `WorkflowExecution`s and `→` represents a `superseded_by` field):
-    - If "A → B → C" and we remove "A", the result will be "B → C".
-    - If "A → B → C" and we remove "B", the result will be "A → C".
-    - If "A → B → C" and we remove "C", the result will be "A → B".
+    Two scenarios:
+    1. If no `WorkflowExecution` supersedes the subject `WorkflowExecution`, this function will
+       delete the `superseded_by` field of each `WorkflowExecution` and `DataObject` that is
+       superseded by the subject one; for example:
+       If "WFE1 → WFE2" and "DO1 → WFE2" and the subject is "WFE2", this
+       function will delete the `superseded_by` field from "WFE1" and "DO1".
+    2. If a `WorkflowExecution` does supersede the subject `WorkflowExecution`, this function will
+       update the `superseded_by` field of each `WorkflowExecution` and `DataObject` that is
+       superseded by the subject one, so that that field contains the `id` of
+       the `WorkflowExecution` that supersedes the subject one; for example:
+       If "WFE1 → WFE2 → WFE3" and "DO1 → WFE2 → WFE3" and the subject is "WFE2", this
+       function will update the `superseded_by` field of "WFE1" and "DO1" to point to "WFE3".
 
-    :param workflow_execution: Dictionary representing the `WorkflowExecution` you want to remove
-                               from any supersession chains it belongs to.
+    :param workflow_execution: Dictionary representing the `WorkflowExecution` for whose deletion
+                               you want to prepare the supersession chain. We call this the
+                               "subject" `WorkflowExecution`.
     :param db: Database instance to use for updating `superseded_by` fields.
     :param client_session: Optional `ClientSession`, which the function will use to perform the
                            database operations within the existing MongoDB transaction, if any.
@@ -30,7 +39,7 @@ def remove_from_supersession_chain(
 
     # Get the `id` of the `WorkflowExecution`, if any, that supersedes the subject one.
     subject_workflow_execution_id = workflow_execution["id"]
-    wfe_superseding_subject_workflow_execution = (
+    id_of_wfe_superseding_subject_workflow_execution = (
         workflow_execution["superseded_by"]
         if "superseded_by" in workflow_execution
         else None
@@ -40,27 +49,14 @@ def remove_from_supersession_chain(
     workflow_execution_set = db.get_collection("workflow_execution_set")
     data_object_set = db.get_collection("data_object_set")
 
-    # Update the `superseded_by` fields of all directly-upstream (i.e. directly-superseded)
-    # `WorkflowExecution`s (WFEs) and `DataObject`s (DOs) to reflect the impending removal
-    # of the subject `WorkflowExecution` from the supersession chain.
-    #
-    # Scenario 1: If no `WorkflowExecution` supersedes the subject `WorkflowExecution`, we will
-    #             delete the `superseded_by` fields from WFEs and DOs that are superseded by
-    #             the subject one; i.e. if "WFE1 → WFE2" and "DO1 → WFE2" and the subject is "WFE2",
-    #             we will delete the `superseded_by` field from "WFE1" and "DO1".
-    #
-    # Scenario 2: If a `WorkflowExecution` supersedes the subject `WorkflowExecution`, we will
-    #             update the `superseded_by` fields of all WFEs and DOs that are superseded by
-    #             the subject one, so those fields contain the `id` of the `WorkflowExecution`
-    #             that supersedes the subject one; i.e. if "WFE1 → WFE2 → WFE3" and
-    #             "DO1 → WFE2 → WFE3" and the subject is "WFE2", we will update
-    #             the `superseded_by` field of "WFE1" and "DO1" to be "WFE3".
-    #
-    if wfe_superseding_subject_workflow_execution is None:
+    # Update or delete the `superseded_by` field of each `WorkflowExecution` and `DataObject`
+    # that is currently superseded by the subject `WorkflowExecution`, so that that field
+    # no longer points to that `WorkflowExecution`.
+    if id_of_wfe_superseding_subject_workflow_execution is None:
         operation = {"$unset": {"superseded_by": ""}}
     else:
         operation = {
-            "$set": {"superseded_by": wfe_superseding_subject_workflow_execution}
+            "$set": {"superseded_by": id_of_wfe_superseding_subject_workflow_execution}
         }
     workflow_execution_set.update_many(
         {"superseded_by": subject_workflow_execution_id},
@@ -70,13 +66,5 @@ def remove_from_supersession_chain(
     data_object_set.update_many(
         {"superseded_by": subject_workflow_execution_id},
         operation,
-        session=client_session,
-    )
-
-    # Remove the `superseded_by` field, if any, from the subject `WorkflowExecution`,
-    # effectively removing that `WorkflowExecution` from the supersession chain, if any.
-    workflow_execution_set.update_one(
-        {"id": subject_workflow_execution_id},
-        {"$unset": {"superseded_by": ""}},
         session=client_session,
     )
