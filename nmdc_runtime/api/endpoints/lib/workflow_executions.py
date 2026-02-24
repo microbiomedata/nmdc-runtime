d1c23b279b39251fd2fa84179965def9a21f1102
eecavanna <eecavanna@users.noreply.github.com>
2026-02-23 18:23:16 -0800

    Also update `superseded_by` field of superseded `DataObject`s

--- nmdc_runtime/api/endpoints/lib/workflow_executions.py
+++ nmdc_runtime/api/endpoints/lib/workflow_executions.py
@@ -11,7 +11,8 @@ def remove_from_supersession_chain(
 ) -> None:
     """
     Removes the specified `WorkflowExecution` from any `superseded_by` chain(s) it belongs to,
-    updating the `superseded_by` fields of upstream `WorkflowExecution`s accordingly.
+    updating the `superseded_by` fields of upstream `WorkflowExecution`s and `DataObject`s
+    accordingly.
 
     Examples (where A/B/C represent `WorkflowExecution`s and `→` represents a `superseded_by` field):
     - If "A → B → C" and we remove "A", the result will be "B → C".
@@ -28,31 +29,38 @@ def remove_from_supersession_chain(
     """
 
     # Get the `id` of the `WorkflowExecution`, if any, that supersedes the subject one.
-    wfe_id = workflow_execution["id"]
-    wfe_superseding_subject_wfe = (
+    subject_workflow_execution_id = workflow_execution["id"]
+    wfe_superseding_subject_workflow_execution = (
         workflow_execution["superseded_by"]
         if "superseded_by" in workflow_execution
         else None
     )
 
-    # Remove the `superseded_by` field, if any, from the subject `WorkflowExecution`,
-    # effectively removing it from the chain, if any.
-    wfe_set = db.get_collection("workflow_execution_set")
-    wfe_set.update_one(
-        {"id": wfe_id}, {"$unset": {"superseded_by": ""}}, session=client_session
-    )
+    # Get references to relevant MongoDB collections.
+    workflow_execution_set = db.get_collection("workflow_execution_set")
+    data_object_set = db.get_collection("data_object_set")
 
-    # Update the `superseded_by` fields to reflect the removal of the subject WFE from the chain.
+    # Update the `superseded_by` fields of all upstream (superseded) `WorkflowExecution`s (WFEs)
+    # and `DataObject`s (DOs), if any, to reflect the removal of the subject WFE from the chain.
     #
-    # If the subject WFE is not superseded by any other WFE, we will just delete the `superseded_by`
-    # field from all WFEs that are superseded by the subject one.
+    # Scenario 1: If the subject WFE is _not_ `superseded_by` any WFE or DO, we will just delete the
+    #             `superseded_by` fields from all WFEs/DOs that are superseded by the subject one.
+    #             i.e., if "WFE1 → WFE2" and subject is "WFE2", delete the field from "WFE1".
     #
-    # If the subject WFE is superseded by another WFE, we will update the `superseded_by` field of
-    # all WFEs that are superseded by the subject one, so they contain the `id` of the WFE that
-    # supersedes the subject one.
+    # Scenario 2: If the subject WFE _is_ `superseded_by` any WFE or DO, we will update the
+    #             `superseded_by` fields of all WFEs/DOs that are `superseded_by` the subject one,
+    #             so those fields contain the `id` of the WFE that supersedes the subject one.
+    #             i.e., if "WFE1 → WFE2 → WFE3" and subject is "WFE2", update the field on "WFE1".
     #
-    if wfe_superseding_subject_wfe is None:
+    if wfe_superseding_subject_workflow_execution is None:
         operation = {"$unset": {"superseded_by": ""}}
     else:
-        operation = {"$set": {"superseded_by": wfe_superseding_subject_wfe}}
-    wfe_set.update_many({"superseded_by": wfe_id}, operation, session=client_session)
+        operation = {"$set": {"superseded_by": wfe_superseding_subject_workflow_execution}}
+    workflow_execution_set.update_many({"superseded_by": subject_workflow_execution_id}, operation, session=client_session)
+    data_object_set.update_many({"superseded_by": subject_workflow_execution_id}, operation, session=client_session)
+
+    # Remove the `superseded_by` field, if any, from the subject `WorkflowExecution`,
+    # effectively removing that `WorkflowExecution` from the supersession chain, if any.
+    workflow_execution_set.update_one(
+        {"id": subject_workflow_execution_id}, {"$unset": {"superseded_by": ""}}, session=client_session
+    )
