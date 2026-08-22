d3c3d4983df49801a89d00d926edc5afa047d3e2
eecavanna <134325062+eecavanna@users.noreply.github.com>
2026-08-21 23:22:23 -0700

    Extract NEON-related Dagster ops into separate Python module

---
+++ nmdc_runtime/site/ops_pkg/neon.py
@@ -0,0 +1,204 @@
+"""
+Dagster ops related to NEON.
+
+Note: These were extracted from a 1900-line file at `nmdc_runtime/site/ops.py` during a refactor.
+"""
+
+from typing import Optional, Set, Tuple
+
+import pandas as pd
+import requests
+from dagster import OpExecutionContext, Out, op
+from nmdc_schema import nmdc
+
+from nmdc_runtime.site.resources import RuntimeApiSiteClient, NeonApiClient
+from nmdc_runtime.site.translation.neon_soil_translator import NeonSoilDataTranslator
+from nmdc_runtime.site.translation.neon_benthic_translator import (
+    NeonBenthicDataTranslator,
+)
+from nmdc_runtime.site.translation.neon_surface_water_translator import (
+    NeonSurfaceWaterDataTranslator,
+)
+
+
+@op(config_schema={"dna_sample_ids": [str]})
+def get_neon_soil_sample_filter(context: OpExecutionContext) -> Set[str]:
+    dna_sample_ids = set(context.op_config["dna_sample_ids"])
+    context.log.info(f"Filtering to {len(dna_sample_ids)} dnaSampleIDs")
+    return dna_sample_ids
+
+
+@op(config_schema={"mms_data_product": dict})
+def get_neon_pipeline_mms_data_product(context: OpExecutionContext) -> dict:
+    return context.op_config["mms_data_product"]
+
+
+@op(config_schema={"sls_data_product": dict})
+def get_neon_pipeline_sls_data_product(context: OpExecutionContext) -> dict:
+    return context.op_config["sls_data_product"]
+
+
+@op(config_schema={"benthic_data_product": dict})
+def get_neon_pipeline_benthic_data_product(context: OpExecutionContext) -> dict:
+    return context.op_config["benthic_data_product"]
+
+
+@op(config_schema={"surface_water_data_product": dict})
+def get_neon_pipeline_surface_water_data_product(context: OpExecutionContext) -> dict:
+    return context.op_config["surface_water_data_product"]
+
+
+@op(required_resource_keys={"neon_api_client"})
+def neon_data_by_product(
+    context: OpExecutionContext, data_product: dict
+) -> dict[str, pd.DataFrame]:
+    df_dict = {}
+    client: NeonApiClient = context.resources.neon_api_client
+
+    product_id = data_product["product_id"]
+    product_tables = data_product["product_tables"]
+
+    product_table_list = [t.strip() for t in product_tables.split(",")]
+    product = client.fetch_product_by_id(product_id)
+    for table_name in product_table_list:
+        df = pd.DataFrame()
+        for site in product["data"]["siteCodes"]:
+            for data_url in site["availableDataUrls"]:
+                data_files = client.request(data_url)
+                for file in data_files["data"]["files"]:
+                    if table_name in file["name"] and "expanded" in file["name"]:
+                        current_df = pd.read_csv(file["url"])
+                        df = pd.concat([df, current_df], ignore_index=True)
+        df_dict[table_name] = df
+
+    return df_dict
+
+
+@op(required_resource_keys={"runtime_api_site_client"})
+def nmdc_schema_database_from_neon_soil_data(
+    context: OpExecutionContext,
+    mms_data: dict[str, pd.DataFrame],
+    sls_data: dict[str, pd.DataFrame],
+    site_code_mapping: dict[str, str],
+    neon_envo_mappings_file: pd.DataFrame,
+    neon_raw_data_file_mappings_file: pd.DataFrame,
+    neon_nmdc_instrument_mapping_file: pd.DataFrame,
+    allowed_dna_sample_ids: Optional[Set[str]],
+) -> nmdc.Database:
+    client: RuntimeApiSiteClient = context.resources.runtime_api_site_client
+
+    def id_minter(*args, **kwargs):
+        response = client.mint_id(*args, **kwargs)
+        return response.json()
+
+    translator = NeonSoilDataTranslator(
+        mms_data,
+        sls_data,
+        neon_envo_mappings_file,
+        neon_raw_data_file_mappings_file,
+        site_code_mapping,
+        neon_nmdc_instrument_mapping_file,
+        allowed_dna_sample_ids=allowed_dna_sample_ids,
+        id_minter=id_minter,
+    )
+
+    database = translator.get_database()
+    return database
+
+
+@op(required_resource_keys={"runtime_api_site_client"})
+def nmdc_schema_database_from_neon_benthic_data(
+    context: OpExecutionContext,
+    benthic_data: dict[str, pd.DataFrame],
+    site_code_mapping: dict[str, str],
+    neon_envo_mappings_file: pd.DataFrame,
+    neon_raw_data_file_mappings_file: pd.DataFrame,
+    neon_nmdc_instrument_mapping_file: pd.DataFrame,
+) -> nmdc.Database:
+    client: RuntimeApiSiteClient = context.resources.runtime_api_site_client
+
+    def id_minter(*args, **kwargs):
+        response = client.mint_id(*args, **kwargs)
+        return response.json()
+
+    translator = NeonBenthicDataTranslator(
+        benthic_data,
+        site_code_mapping,
+        neon_envo_mappings_file,
+        neon_raw_data_file_mappings_file,
+        neon_nmdc_instrument_mapping_file,
+        id_minter=id_minter,
+    )
+
+    database = translator.get_database()
+    return database
+
+
+@op(required_resource_keys={"runtime_api_site_client"})
+def nmdc_schema_database_from_neon_surface_water_data(
+    context: OpExecutionContext,
+    surface_water_data: dict[str, pd.DataFrame],
+    site_code_mapping: dict[str, str],
+    neon_envo_mappings_file: pd.DataFrame,
+    neon_raw_data_file_mappings_file: pd.DataFrame,
+    neon_nmdc_instrument_mapping_file: pd.DataFrame,
+) -> nmdc.Database:
+    client: RuntimeApiSiteClient = context.resources.runtime_api_site_client
+
+    def id_minter(*args, **kwargs):
+        response = client.mint_id(*args, **kwargs)
+        return response.json()
+
+    translator = NeonSurfaceWaterDataTranslator(
+        surface_water_data,
+        site_code_mapping,
+        neon_envo_mappings_file,
+        neon_raw_data_file_mappings_file,
+        neon_nmdc_instrument_mapping_file,
+        id_minter=id_minter,
+    )
+
+    database = translator.get_database()
+    return database
+
+
+@op(
+    out={
+        "neon_envo_mappings_file_url": Out(),
+        "neon_raw_data_file_mappings_file_url": Out(),
+        "neon_nmdc_instrument_mapping_file_url": Out(),
+    }
+)
+def get_neon_pipeline_inputs(
+    neon_envo_mappings_file_url: str,
+    neon_raw_data_file_mappings_file_url: str,
+    neon_nmdc_instrument_mapping_file_url: str,
+) -> Tuple[str, str, str]:
+    return (
+        neon_envo_mappings_file_url,
+        neon_raw_data_file_mappings_file_url,
+        neon_nmdc_instrument_mapping_file_url,
+    )
+
+@op
+def nmdc_schema_database_export_filename_neon() -> str:
+    return "database_from_neon_metadata.json"
+
+
+@op
+def site_code_mapping() -> dict:
+    endpoint = "https://data.neonscience.org/api/v0/sites/"
+    response = requests.get(endpoint)
+    if response.status_code == 200:
+        sites_data = response.json()
+        site_code_mapping = {
+            site["siteCode"]: f"USA: {site['stateName']}, {site['siteName']}".replace(
+                " NEON", ""
+            )
+            for site in sites_data["data"]
+        }
+        return site_code_mapping
+    else:
+        raise Exception(
+            f"Failed to fetch site data from {endpoint}. Status code: {response.status_code}, Content: {response.content}"
+        )
